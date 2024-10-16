36f4a42f4834e9d04026c0fd3190d3d308a6056c
adkipnis <adkipnis@mailbox.org>
2024-10-16 16:38:16 +0200

    delete outdates functions

--- train.py
+++ train.py
@@ -12,22 +12,6 @@ from model import Transformer
 from config import getConfig, getWeightsFilePath
 
 
-def getOrBuildTokenizer(config: dict, dataset, language) -> Tokenizer:
-    tokenizer_path = Path(config["tokenizer_file"].format(language))
-    if not Path.exists(tokenizer_path):
-        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
-        tokenizer.pre_tokenizer = Whitespace()
-        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
-        tokenizer.train_from_iterator(getAllSentences(dataset, language), trainer)
-        tokenizer.save(str(tokenizer_path))
-    else:
-        tokenizer = Tokenizer.from_file(str(tokenizer_path))
-    return tokenizer
-
-def getDataset(config: dict) -> Tuple[DataLoader, DataLoader, Tokenizer, Tokenizer]:
-    dataset = load_dataset("opus_books", "de-en", split="train")
-    tokenizer_src = getOrBuildTokenizer(config, dataset, "de")
-    tokenizer_tgt = getOrBuildTokenizer(config, dataset, "en")
 
     # train validation split
     train_ds_size = int(0.9 * len(dataset))
