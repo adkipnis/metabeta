1f5145138f9c072f798102e88766c6af32ed6ce0
adkipnis <adkipnis@mailbox.org>
2024-10-28 15:49:57 +0100

    adaptive config

--- config.py
+++ config.py
@@ -3,27 +3,27 @@ from datetime import datetime
 timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 def getConfig() -> dict:
-    return {
-            "n_predictors": 0, # number of linear model features (without bias)
-            "n_samples": 64, # number of subjects per draw
-            "n_draws": 1000, # number of draws per epoch
-            "batch_size": 10, # number of concurrent draws per batch
+    cfg = {
+            "n_predictors": 1, # number of linear model features (without bias)
+            "n_draws": 1000, # number of datasets per epoch
+            "batch_size": 10, # number of datasets per batch
             "n_epochs": 500, # number of epochs
             "lr": 1e-3, # learning rate
             "d_model": 64, # size of embedding layer
             "d_ff": 128, # size of feed forward layer
             "n_heads": 2, # number of heads
-            "n_blocks_e": 3, # number of encoder blocks
-            "n_blocks_d": 3, # number of decoder blocks
+            "n_blocks_e": 2, # number of encoder blocks
+            "n_blocks_d": 4, # number of decoder blocks
             "dropout": 0.1, # dropout rate
             "seq_len": 2000, # max length of sequence
-            "seq_len_out": 4*3 + 2, # max length of output sequence
             "precision": 4, # tokenizer float representation
-            "preload": None, # None or epoch number
+            "preload": 10, # None or epoch number
             "model_folder": "weights",
             "model_basename": "tmodel",
-            "experiment_name": f"runs/linear/{timestamp}",
             }
+    cfg["seq_len_out"] = (1 + cfg["n_predictors"]) * 3 + 2
+    cfg["experiment_name"] = f"runs/linear-{cfg['n_predictors']}/{timestamp}"
+    return cfg
 
 def getWeightsFilePath(config: dict, epoch: int):
     model_folder = config["model_folder"]
