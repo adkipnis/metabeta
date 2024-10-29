fc988e2a54092c68421d37dea4f324bb9247d7e0
adkipnis <adkipnis@mailbox.org>
2024-10-29 16:12:23 +0100

    update cfg

--- config.py
+++ config.py
@@ -5,24 +5,25 @@ timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
 def getConfig() -> dict:
     cfg = {
             "n_predictors": 1, # number of linear model features (without bias)
-            "n_draws": 1000, # number of datasets per epoch
-            "batch_size": 10, # number of datasets per batch
-            "n_epochs": 500, # number of epochs
+            "n_draws": 1024, # number of datasets per epoch
+            "batch_size": 128, # number of datasets per batch
+            "n_epochs": 1000, # number of epochs
             "lr": 1e-3, # learning rate
             "d_model": 64, # size of embedding layer
             "d_ff": 128, # size of feed forward layer
+            "d_out": 16, # size of output layer
             "n_heads": 2, # number of heads
             "n_blocks_e": 2, # number of encoder blocks
             "n_blocks_d": 4, # number of decoder blocks
             "dropout": 0.1, # dropout rate
             "seq_len": 2000, # max length of sequence
             "precision": 4, # tokenizer float representation
-            "preload": 10, # None or epoch number
+            "preload": None, # None or epoch number
             "model_folder": "weights",
-            "model_basename": "tmodel",
+            "model_basename": "tmodel-numeric",
             }
     cfg["seq_len_out"] = (1 + cfg["n_predictors"]) * 3 + 2
-    cfg["experiment_name"] = f"runs/linear-{cfg['n_predictors']}/{timestamp}"
+    cfg["experiment_name"] = f"runs/linear-mse-{cfg['n_predictors']}/{timestamp}"
     return cfg
 
 def getWeightsFilePath(cfg: dict, epoch: int):
