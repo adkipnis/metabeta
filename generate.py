2d4133f3e6edb64c2bed6efdd0d2f3a8e33972de
adkipnis <adkipnis@mailbox.org>
2024-11-06 14:12:39 +0100

    get N

--- generate.py
+++ generate.py
@@ -24,3 +24,11 @@ def getD(seed: int, max_predictors: int) -> int:
     return int(d.item())
 
 
+def getN(seed:int, n_predictors: int, max_samples: int) -> int:
+    ''' Get a random number of samples to draw from a linear model, depending on the number of predictors.'''
+    torch.manual_seed(seed)
+    low = 5 * (n_predictors + 1)
+    n = torch.randint(low, max_samples, (1,))
+    return int(n.item())
+
+
