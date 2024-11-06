7855c8569ace96e292cdc280a9b9b1890bb7364e
adkipnis <adkipnis@mailbox.org>
2024-11-06 14:08:16 +0100

    initialize weights

--- rnn.py
+++ rnn.py
@@ -13,6 +13,15 @@ class BaseRNN(nn.Module):
         self.linear = nn.Linear(hidden_size, output_size)
         self.seed = seed
         self.initializeWeights()
+
+    def initializeWeights(self) -> None:
+        seed = self.seed
+        for p in self.parameters():
+            if p.dim() > 1:
+                torch.manual_seed(seed)
+                seed += 1
+                nn.init.xavier_uniform_(p)
+
     def forward(self, x: torch.Tensor, lengths: List[int]) -> torch.Tensor:
         # Pack the padded sequence
         packed_input = pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False) # type: ignore
