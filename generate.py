6ea1ca1480905c4ee03726a81b9d9cf5bf51eb9c
adkipnis <adkipnis@mailbox.org>
2024-11-06 14:12:26 +0100

    sower class

--- generate.py
+++ generate.py
@@ -6,3 +6,14 @@ from tasks import Task, LinearModel
 from dataset import RnnDataset
 
 
+class Sower:
+    ''' Seed generator for reproducibility. '''
+    def __init__(self, seed: int = 0) -> None:
+        self.seed = seed
+
+    def throw(self) -> int:
+        out = self.seed
+        self.seed += 1
+        return out
+
+
