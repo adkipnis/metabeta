6f6366cf798aa0f79cf5858176a74ba8efb0678a
adkipnis <adkipnis@mailbox.org>
2024-11-06 15:37:16 +0100

    get console width

--- train.py
+++ train.py
@@ -12,3 +12,15 @@ from rnn import GRU
 from generate import dsFilename
 
 
+def getConsoleWidth() -> int:
+    ''' Determine the width of the console for formatting purposes. '''
+    try:
+        with os.popen('stty size', 'r') as console:
+            _, console_width = console.read().split()
+            console_width = int(console_width)
+    except:
+        print("Could not determine console width. Defaulting to 80.")
+        console_width = 80
+    return console_width
+
+
