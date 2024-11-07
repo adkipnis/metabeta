dafd3ad30e24c9de7fc662d621142955334074ac
adkipnis <adkipnis@mailbox.org>
2024-11-07 19:25:48 +0100

    reuse base class as much as possible

--- models.py
+++ models.py
@@ -7,17 +7,15 @@ from torch.nn import TransformerDecoderLayer
 class Base(nn.Module):
     def __init__(self,
                  input_size: int,
-                 embed_size: int,
                  hidden_size: int,
                  output_size: int,
                  seed: int,
                  reuse: bool = True) -> None:
         super(Base, self).__init__()
         self.input_size = input_size
-        self.embed_size = embed_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.embedding = nn.Linear(input_size, embed_size)
+        self.embedding = nn.Linear(input_size, hidden_size)
         self.means = nn.Linear(hidden_size, output_size)
         self.logstds = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
@@ -34,8 +32,8 @@ class Base(nn.Module):
                 nn.init.xavier_uniform_(p)
 
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        # x (batch_size, seq_size, d+1)
-        x = self.embedding(x) # (batch_size, seq_size, embedding_size)
+        # x (batch_size, seq_size, input_size)
+        x = self.embedding(x) # (batch_size, seq_size, hidden_size)
         outputs = self.internal(x) # (batch_size, seq_size, hidden_size)
 
         # Forward pass through TransformerDecoder
@@ -49,33 +47,33 @@ class Base(nn.Module):
 
 
 class RNN(Base):
-    def __init__(self, input_size: int, embed_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
-        super(RNN, self).__init__(input_size, embed_size, hidden_size, output_size, seed, reuse)
-        self.model = nn.RNN(input_size=embed_size, hidden_size=hidden_size, batch_first=True)
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
+        super(RNN, self).__init__(input_size, hidden_size, output_size, seed, reuse)
+        self.model = nn.RNN(input_size=hidden_size, hidden_size=hidden_size, batch_first=True)
 
     def internal(self, x: torch.Tensor) -> torch.Tensor:
-        # x (batch_size, seq_size, embed_size)
+        # x (batch_size, seq_size, hidden_size)
         outputs, _ = self.model(x)
         return outputs
 
 
 class GRU(RNN):
-    def __init__(self, input_size: int, embed_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
-        super(GRU, self).__init__(input_size, embed_size, hidden_size, output_size, seed, reuse)
-        self.model = nn.GRU(input_size=input_size, hidden_size=hidden_size, batch_first=True)
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
+        super(GRU, self).__init__(input_size, hidden_size, output_size, seed, reuse)
+        self.model = nn.GRU(input_size=hidden_size, hidden_size=hidden_size, batch_first=True)
 
 
 class LSTM(RNN):
-    def __init__(self, input_size: int, embed_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
-        super(LSTM, self).__init__(input_size, embed_size, hidden_size, output_size, seed, reuse)
-        self.model = nn.LSTM(input_size=input_size, hidden_size=hidden_size, batch_first=True)
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
+        super(LSTM, self).__init__(input_size, hidden_size, output_size, seed, reuse)
+        self.model = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, batch_first=True)
 
 
 class TransformerDecoder(Base):
     def __init__(self,
                  input_size: int,
-                 embed_size: int,
                  hidden_size: int,
+                 ff_size: int,
                  output_size: int,
                  seed: int,
                  reuse: bool = True,
@@ -83,12 +81,12 @@ class TransformerDecoder(Base):
                  num_layers: int = 1,
                  ) -> None:
 
-        super(TransformerDecoder, self).__init__(input_size, embed_size, hidden_size, output_size, seed, reuse)
-        decoder_layer = TransformerDecoderLayer(d_model=embed_size, nhead=nhead, dim_feedforward=hidden_size, batch_first=True)
+        super(TransformerDecoder, self).__init__(input_size, hidden_size, output_size, seed, reuse)
+        decoder_layer = TransformerDecoderLayer(d_model=hidden_size, nhead=nhead, dim_feedforward=ff_size, batch_first=True)
         self.model= nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
 
     def internal(self, x: torch.Tensor) -> torch.Tensor:
-        # x (batch_size, seq_size, embed_size)
+        # x (batch_size, seq_size, hidden_size)
         return self.model(x, x)
 
 
