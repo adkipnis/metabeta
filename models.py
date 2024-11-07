086b91afe82e5cc75823100955d9ae9d06e3cea2
adkipnis <adkipnis@mailbox.org>
2024-11-07 18:55:30 +0100

    rename and restructure

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from torch.nn import TransformerDecoderLayer
 
 class Base(nn.Module):
-    def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True, pack: bool = False) -> None:
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
         super(Base, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -14,8 +14,7 @@ class Base(nn.Module):
         self.logstds = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
         self.seed = seed
-        self.reuse = reuse
-        self.pack = pack
+        self.reuse = reuse # reuse intermediate outputs
         self.initializeWeights()
 
     def initializeWeights(self) -> None:
@@ -25,6 +24,12 @@ class Base(nn.Module):
                 torch.manual_seed(seed)
                 seed += 1
                 nn.init.xavier_uniform_(p)
+
+
+class RNN(Base):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True, pack: bool = False) -> None:
+        super(RNN, self).__init__(input_size, hidden_size, output_size, seed, reuse)
+        self.pack = pack
 
     def forward(self, x: torch.Tensor, lengths: Union[None, List[int]] = None) -> Tuple[torch.Tensor, torch.Tensor]:
         # optionally pack the padded sequence
@@ -48,50 +53,47 @@ class Base(nn.Module):
         return means, logstds
 
 
-class GRU(Base):
+class GRU(RNN):
     def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
         super(GRU, self).__init__(input_size, hidden_size, output_size, seed, reuse)
         self.model = nn.GRU(input_size=input_size, hidden_size=hidden_size, batch_first=True)
 
 
-class LSTM(Base):
+class LSTM(RNN):
     def __init__(self, input_size: int, hidden_size: int, output_size: int, seed: int, reuse: bool = True) -> None:
         super(LSTM, self).__init__(input_size, hidden_size, output_size, seed, reuse)
         self.model = nn.LSTM(input_size=input_size, hidden_size=hidden_size, batch_first=True)
 
 
 class TransformerDecoder(Base):
-    def __init__(self, input_size: int, hidden_size: int, ff_size: int, output_size: int, seed: int, reuse: bool = True, nhead: int = 4, num_layers: int = 1) -> None:
+    def __init__(self,
+                 input_size: int,
+                 hidden_size: int,
+                 ff_size: int,
+                 output_size: int,
+                 seed: int,
+                 reuse: bool = True,
+                 nhead: int = 4,
+                 num_layers: int = 1,
+                 ) -> None:
+
         super(TransformerDecoder, self).__init__(input_size, hidden_size, output_size, seed, reuse)
-        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.ff_size = ff_size
+        self.embedding = nn.Linear(input_size, hidden_size)
         decoder_layer = TransformerDecoderLayer(d_model=hidden_size, nhead=nhead, dim_feedforward=ff_size, batch_first=True)
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
         self.output_linear = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x: torch.Tensor, lengths: Union[None, List[int]] = None) -> Tuple[torch.Tensor, torch.Tensor]:
-        # Optionally mask the padded sequence and forward pass through TransformerDecoder
-        if self.pack and isinstance(lengths, list):
-            mask = self.paddingMask(x, lengths)
-            outputs = self.transformer_decoder(x, x, tgt_key_padding_mask=mask, memory_key_padding_mask=mask)
-        else:
-            outputs = self.transformer_decoder(x, x) # (batch_size, seq_len, hidden_size)
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        x = self.embedding(x) # (batch_size, seq_size, d+1)
+        outputs = self.transformer_decoder(x, x) # (batch_size, seq_size, hidden_size)
 
         # Forward pass through TransformerDecoder
         if not self.reuse:
             outputs = outputs[:, -1]
         
-        # Linear transformation
-        outputs = self.output_linear(outputs)
-        
         # Transform outputs
         means = self.means(self.relu(outputs))
         logstds = self.logstds(self.relu(outputs))
         return means, logstds
-
-
-    def paddingMask(self, x: torch.Tensor, lengths: List[int]) -> torch.Tensor:
-        mask = torch.zeros(x.size(0), x.size(1), dtype=torch.bool, device=x.device)
-        for i, length in enumerate(lengths):
-            mask[i, length:] = True
-        return mask
 
