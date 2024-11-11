24500a2d2dbdace4357111242001a8711ac1802d
adkipnis <adkipnis@mailbox.org>
2024-11-11 15:44:22 +0100

    import padding functions

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import TransformerDecoderLayer
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 class Base(nn.Module):
     def __init__(self,
