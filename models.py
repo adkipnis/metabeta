d4832ce3efe9ccf1b1dc2c3851d906c5522c28d6
adkipnis <adkipnis@mailbox.org>
2024-11-12 15:49:58 +0100

    remove legacy code for "last datapoint"

--- models.py
+++ models.py
@@ -1,9 +1,7 @@
 from typing import Tuple
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.nn import TransformerDecoderLayer
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 class Base(nn.Module):
     def __init__(self,
@@ -11,8 +9,7 @@ class Base(nn.Module):
                  hidden_size: int,
                  n_layers: int,
                  dropout: float,
-                 seed: int,
-                 last: bool = False) -> None:
+                 seed: int) -> None:
         super(Base, self).__init__()
         self.input_size = num_predictors + 1
         self.hidden_size = hidden_size
@@ -20,11 +17,8 @@ class Base(nn.Module):
         self.n_layers = n_layers
         self.dropout = dropout
         self.means = nn.Linear(hidden_size, self.output_size)
-        # self.logstds = nn.Linear(hidden_size, output_size * (output_size + 1) // 2)
         self.logstds = nn.Linear(hidden_size, self.output_size)
-        self.relu = nn.ReLU()
         self.seed = seed
-        self.last = last
 
     def initializeWeights(self) -> None:
         ''' Initialize weights using Xavier initialization '''
@@ -33,74 +27,29 @@ class Base(nn.Module):
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
 
-    def toCovariance(self, logstds: torch.Tensor) -> torch.Tensor:
-        ''' construct triangular matrix from logstds, apply softplus to diagonal and multiply with its transpose '''
-        # logstds (b, output_size_sigma) or (b, n, output_size_sigma)
-        d = self.output_size # number of predictors
-        b = logstds.size(0) # batch size
-        tril_ids = torch.tril_indices(d, d, offset=0)
-        diag_ids = torch.arange(d)
-        if logstds.dim() == 2:
-            chol_matrix = torch.zeros(b, d, d, device=logstds.device)
-            chol_matrix[:, tril_ids[0], tril_ids[1]] = logstds
-            chol_matrix[:, diag_ids, diag_ids] = F.softplus(chol_matrix[:, diag_ids, diag_ids])
-        else:
-            n = logstds.size(1) # number of samples
-            chol_matrix = torch.zeros(b, n, d, d, device=logstds.device)
-            chol_matrix[:, :, tril_ids[0], tril_ids[1]] = logstds
-            chol_matrix[:, :, diag_ids, diag_ids] = F.softplus(chol_matrix[:, :, diag_ids, diag_ids])
-        cov_matrix = chol_matrix @ chol_matrix.transpose(-1, -2)
-        if logstds.dim() == 2:
-            cov_matrix[:, diag_ids, diag_ids] += 1e-6 # add small value to diagonal
-        else:
-            cov_matrix[:, :, diag_ids, diag_ids] += 1e-6
-        return cov_matrix
-
-    def internal(self, x: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
+    def internal(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
 
-    def forward(self, x: torch.Tensor, lengths: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        ''' forward pass, get all intermediate outputs and map them to the parameters of the proposal posterior
-        x: (batch_size, seq_size, input_size)'''
+    def forward(self, x: torch.Tensor # (batch_size, seq_size, input_size)
+                ) -> Tuple[torch.Tensor, torch.Tensor]:
+        ''' forward pass, get all intermediate outputs and map them to the parameters of the proposal posterior '''
 
         # run through main layers
-        outputs = self.internal(x, lengths) # (batch_size, seq_size, hidden_size) if no masking
+        outputs = self.internal(x) # (batch_size, seq_size, hidden_size)
     
-        # optionally take last output
-        if self.last:
-            batch_size = lengths.size(0)
-            outputs = outputs[torch.arange(batch_size), lengths-1] # (batch_size, hidden_size)
-
         # Transform outputs
-        means = self.means(outputs) # (batch_size, output_size) or (batch_size, seq_size, output_size)
-        logstds = self.logstds(outputs)
-        # sigmas = self.toCovariance(logstds) # (batch_size, output_size, output_size) or (batch_size, seq_size, output_size, output_size)
+        means = self.means(outputs) # (batch_size, seq_size, output_size)
+        logstds = self.logstds(outputs) # (batch_size, seq_size, output_size)
         return means, logstds.exp()
 
 
 class RNNBase(Base):
     def __init__(self, num_predictors: int, hidden_size: int, n_layers: int, dropout: float, seed: int, last: bool = False) -> None:
-        super(RNNBase, self).__init__(num_predictors, hidden_size, n_layers, dropout, seed, last)
+        super(RNNBase, self).__init__(num_predictors, hidden_size, n_layers, dropout, seed)
         self.kwargs = {'input_size': self.input_size, 'hidden_size': hidden_size, 'num_layers': n_layers, 'dropout': dropout, 'batch_first': True}
 
-    def mask(self, x: torch.Tensor, lengths: torch.Tensor) -> torch.nn.utils.rnn.PackedSequence:
-        ''' mask the output of the RNN '''
-        # x (batch_size, seq_size, input_size)
-        return pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
-         
-    def unmask(self, x: torch.nn.utils.rnn.PackedSequence) -> torch.Tensor:
-        ''' unmask the output of the RNN '''
-        output, _ = pad_packed_sequence(x, batch_first=True)
-        return output
-
-    def internal(self, x: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
-        # x (batch_size, seq_size, hidden_size)
-        if self.last:
-            x = self.mask(x, lengths) # type: ignore
-        outputs, hidden = self.model(x)
-        if self.last:
-            # outputs = self.unmask(outputs) # (batch_size, max(lengths), hidden_size)
-            return hidden[-1]
+    def internal(self, x: torch.Tensor) -> torch.Tensor:
+        outputs, _ = self.model(x)
         return outputs
 
 
@@ -126,7 +75,6 @@ class TransformerDecoder(Base):
                  n_layers: int,
                  dropout: float,
                  seed: int,
-                 last: bool = False,
                  n_heads: int = 4,
                  ) -> None:
 
@@ -137,25 +85,17 @@ class TransformerDecoder(Base):
                                                 dropout=dropout,
                                                 batch_first=True)
         self.model= nn.TransformerDecoder(decoder_layer, num_layers=n_layers)
-        self.map2hidden = nn.Linear(self.input_size, hidden_size)
         self.initializeWeights()
 
     def targetMask(self, x: torch.Tensor) -> torch.Tensor:
         seq_len = x.size(1)
         return torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool()
 
-    def paddingMask(self, x: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
-        # mask[i, j] = True if j >= lengths[i] else False
-        batch_size, seq_size, _ = x.size()
-        mask = torch.arange(seq_size).expand(batch_size, seq_size) >= lengths.unsqueeze(1)
-        return mask.bool()
 
     def internal(self, x: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
         # x (batch_size, seq_size, hidden_size)
         tgt_mask = self.targetMask(x)
         tgt_key_padding_mask = None
-        if self.last:
-            tgt_key_padding_mask = self.paddingMask(x, lengths)
         outputs = self.model(tgt=x, memory=x, tgt_mask=tgt_mask, tgt_key_padding_mask=tgt_key_padding_mask)
         return self.map2hidden(outputs)
 
