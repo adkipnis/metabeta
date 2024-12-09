8e04969badbeb73b68a212ff08088fac69e7399f
adkipnis <adkipnis@mailbox.org>
2024-12-09 14:45:30 +0100

    hidden methods

--- tasks.py
+++ tasks.py
@@ -14,30 +14,29 @@ class Task:
         self.data_dist = data_dist
 
         # beta distribution
-        
         self.beta_dist = torch.distributions.Normal(0., math.sqrt(5))
 
         # error distribution
         self.sigma_error = sigma_error
         self.noise_dist = torch.distributions.Normal(0., self.sigma_error)
         
-    def standardize(self, x: torch.Tensor) -> torch.Tensor:
+    def _standardize(self, x: torch.Tensor) -> torch.Tensor:
         mean = torch.mean(x, dim=0)
         sd = torch.std(x, dim=0)
         return (x-mean)/(sd + 1e-8)
 
-    def sampleBetas(self, seed: int) -> torch.Tensor:
+    def _sampleBetas(self, seed: int) -> torch.Tensor:
         torch.manual_seed(seed)
         return self.beta_dist.sample((self.n_predictors+1, 1)) # type: ignore
 
-    def sampleFeatures(self, n_samples: int, seed: int) -> torch.Tensor:
+    def _sampleFeatures(self, n_samples: int, seed: int) -> torch.Tensor:
         torch.manual_seed(seed)
         x = self.data_dist.sample((n_samples, self.n_predictors)) # type: ignore
-        x = self.standardize(x)
+        x = self._standardize(x)
         intercept = torch.ones(n_samples, 1)
         return torch.cat([intercept, x], dim=1)
 
-    def sampleNoise(self, n_samples: int, seed: int) -> torch.Tensor:
+    def _sampleNoise(self, n_samples: int, seed: int) -> torch.Tensor:
         torch.manual_seed(seed)
         return self.noise_dist.sample((n_samples, 1)) # type: ignore
 
@@ -50,9 +49,9 @@ class FixedEffects(Task):
         super().__init__(n_predictors, sigma_error, data_dist)
 
     def sample(self, n_samples: int, seed: int) -> Dict[str, torch.Tensor]:
-        x = self.sampleFeatures(n_samples, seed)
-        betas = self.sampleBetas(seed)
-        eps = self.sampleNoise(n_samples, seed)
+        x = self._sampleFeatures(n_samples, seed)
+        betas = self._sampleBetas(seed)
+        eps = self._sampleNoise(n_samples, seed)
         y = x @ betas + eps
         return {
                 "predictors": x,
