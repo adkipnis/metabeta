9389213236a15c61d00b98c4de0dadcbf0828eee
adkipnis <adkipnis@mailbox.org>
2024-12-12 17:52:26 +0100

    only generate validation set by default

--- generate.py
+++ generate.py
@@ -93,7 +93,7 @@ def setup() -> argparse.Namespace:
     parser.add_argument('-s', '--seed', type=int, default=0, help='Seed for random number generator (default = 0).')
     parser.add_argument('-n', '--n_draws', type=int, default=int(1e4), help='Number of samples to draw per dataset (default = 10,000).')
     parser.add_argument('--n_draws_val', type=int, default=500, help='Number of samples (per d) to draw for validation dataset (default = 500).')
-    parser.add_argument('-i', '--iterations', type=int, default=int(1e3), help='Number of dataset partitions to generate (default = 1,000).')
+    parser.add_argument('-i', '--iterations', type=int, default=0, help='Number of dataset partitions to generate (default = 0, only generates validatioin dataset).')
     parser.add_argument('--max_samples', type=int, default=200, help='Maximum number of samples to draw per linear model (default = 200).')
     parser.add_argument('-d', '--max_predictors', type=int, default=14, help='Maximum number of predictors (without intercept) to draw per linear model (default = 14).')
     parser.add_argument('--start', type=int, default=1, help='Starting iteration number (default = 1).')
