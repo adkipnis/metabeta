2e8d2c77984a915bf9e72e5d907e0c2ea2874656
adkipnis <adkipnis@mailbox.org>
2024-12-13 21:17:55 +0100

    iterator desc inside loop

--- generate.py
+++ generate.py
@@ -62,10 +62,10 @@ def generateBalancedDataset(n_draws_per: int, max_samples: int, max_predictors: 
     data = []
     d = 0 # for iterator description
     iterator = tqdm(range(max_predictors + 1))
-    iterator.set_description(f'{d:02d}/{max_predictors:02d}')
     sigmas = sorted([getSigmaError(i) for i in range(n_draws_per)])
 
     for d in iterator:
+        iterator.set_description(f'{d:02d}/{max_predictors:02d}')
         for seed in range(n_draws_per):
             sigma = math.sqrt(d + 1) * float(sigmas[seed])
             data_dist = getDataDist(seed)
