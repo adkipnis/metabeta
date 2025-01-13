f831b02e4010d98e1c568df01fefb8f5d92bd64d
adkipnis <adkipnis@mailbox.org>
2025-01-13 16:23:31 +0100

    training loss

--- plot.py
+++ plot.py
@@ -11,3 +11,28 @@ from dataset import LMDataset
 # Create a color map from light blue to dark blue
 cmap = colors.LinearSegmentedColormap.from_list("custom_blues", ["#add8e6", "#000080"])
 
+# plot training loss
+def plotTrain(date: str, model_id: str):
+    path = Path('losses', model_id, date, 'loss_train.csv')
+    df = pd.read_csv(path)
+    df = df.groupby(['iteration'])['loss'].agg(['mean', 'std', 'max', 'min']).reset_index()
+    df = df[df['iteration'] != 1]
+    fig, ax = plt.subplots(figsize=(10, 6))
+    
+    ax.plot(df['iteration'], df['mean'], label='Mean Loss', color='blue')
+    
+    # Shade min-max area
+    ax.fill_between(df['iteration'],
+                    # df['mean'] - df['std'], 
+                    # df['mean'] + df['std'], 
+                    df['min'], 
+                    df['max'], 
+                    color='blue', alpha=0.3)
+    
+    plt.xlabel('Iteration')
+    plt.ylabel('-log p(target)')
+    # plt.ylim(-1, 3)
+    plt.grid(True) 
+    plt.show()
+
+
