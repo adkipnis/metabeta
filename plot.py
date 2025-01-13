b2291ce8cce17cfc99133a94e7c1c4620fdf5512
adkipnis <adkipnis@mailbox.org>
2025-01-13 16:23:24 +0100

    cmap

--- plot.py
+++ plot.py
@@ -6,3 +6,8 @@ import numpy as np
 from pathlib import Path
 from typing import Dict
 from dataset import LMDataset
+
+
+# Create a color map from light blue to dark blue
+cmap = colors.LinearSegmentedColormap.from_list("custom_blues", ["#add8e6", "#000080"])
+
