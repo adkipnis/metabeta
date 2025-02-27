7e8178568cd0480b7fdc5a6c04f654440f6cd03e
adkipnis <adkipnis@mailbox.org>
2025-02-27 17:40:21 +0100

    remove old functions
    remove old functions

--- plot.py
+++ plot.py
@@ -16,68 +16,8 @@ cmap = colors.LinearSegmentedColormap.from_list("custom_blues", ["#add8e6", "#00
 # basic plots
 
 
-# plot training loss
-def plotTrain(date: str, model_id: str):
-    path = Path('losses', model_id, date, 'loss_train.csv')
-    df = pd.read_csv(path)
-    df = df.groupby(['iteration'])['loss'].agg(['mean', 'std', 'max', 'min']).reset_index()
-    df = df[df['iteration'] != 1]
-    fig, ax = plt.subplots(figsize=(10, 6))
-    
-    ax.plot(df['iteration'], df['mean'], label='Mean Loss', color='blue')
-    
-    # Shade min-max area
-    ax.fill_between(df['iteration'],
-                    # df['mean'] - df['std'], 
-                    # df['mean'] + df['std'], 
-                    df['min'], 
-                    df['max'], 
-                    color='blue', alpha=0.3)
-    
-    plt.xlabel('Iteration')
-    plt.ylabel('-log p(target)')
-    # plt.ylim(-1, 3)
-    plt.grid(True) 
-    plt.show()
 
 
-# plot validation loss
-def plotVal(date: str, model_id: str, suffix: str = "val", focus: int = -1):
-    path = Path('losses', model_id, date, f'loss_{suffix}.csv')
-    if suffix == "val":
-        ylabel = '-log p(target)' 
-    elif suffix == "kl":
-        ylabel = 'KL Divergence'
-    else:
-        raise ValueError
-    df = pd.read_csv(path)
-    unique_partitions = sum(df['iteration'] == 1)
-    i = df.shape[0] // unique_partitions
-    unique_d = unique_partitions // 5
-    d_values = np.repeat(np.arange(unique_d), 5)
-    df['d'] = np.tile(d_values, i)
-    df_agg = df.groupby(['d', 'iteration'])['loss'].agg(['mean', 'std', 'min', 'max']).reset_index()
-    if focus >= 0:
-        df_agg = df_agg[df_agg['d'] == focus]
-    norm = colors.Normalize(vmin=0, vmax=unique_d)
-    
-    # Create the plot
-    fig, ax = plt.subplots(figsize=(10, 6))
-    
-    for d_value, group in df_agg.groupby('d'):
-        color = cmap(norm(d_value))
-        ax.plot(group['iteration'], group['mean'], label=f'd={d_value}', color=color)
-        ax.fill_between(group['iteration'], 
-                        # group['mean'] - group['std'], 
-                        # group['mean'] + group['std'], 
-                        group['min'],
-                        group['max'],
-                        color=color, alpha=0.3)  # Shade ± SD
-    plt.xlabel('Iteration')
-    plt.ylabel(ylabel)
-    plt.legend()
-    plt.grid(True) 
-    plt.show()
 
 
 # -----------------------------------------------------------------------------------------
@@ -125,19 +65,6 @@ def preloadPredictions(date: str, model_id: str, iteration: int = 100, n_batches
     #     s_emp = torch.stack([x["S_emp"].sqrt() for x in ds_val], dim=0).numpy()
     #     out.update({"s": s, "s_emp": s_emp})
     return {k: v for d in out for k, v in d.items()}
-
-
-# def mixMean(locs: np.ndarray,
-#             weights: np.ndarray) -> np.ndarray:
-#     return np.sum(locs * weights, axis=-1)
-
-
-# def mixVariance(locs: np.ndarray,
-#                 scales: np.ndarray,
-#                 weights: np.ndarray,
-#                 mean: np.ndarray) -> np.ndarray:
-#     second_moments = np.square(locs) + np.square(scales)
-#     return np.sum(second_moments * weights, axis=-1) - np.square(mean)
 
 
 def multivariateDataFrame(loc: torch.Tensor, quants: torch.Tensor) -> pd.DataFrame:
