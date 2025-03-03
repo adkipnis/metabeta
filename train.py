90dbf425efb69c168fb898fd73d33cb0f7288fc2
adkipnis <adkipnis@mailbox.org>
2025-03-03 14:49:15 +0100

    update getResiduals

--- train.py
+++ train.py
@@ -365,9 +365,10 @@ def assembleInputs(y: torch.Tensor,  X: torch.Tensor) -> torch.Tensor:
 
 def getResiduals(y: torch.Tensor, X: torch.Tensor, outputs: Dict[str, torch.Tensor]) -> torch.Tensor:
     locs = outputs["ffx_loc"] 
+    scales = outputs["ffx_scale"]
     weights = outputs["ffx_weight"]
-    loc = mixMean(locs, weights)
-    y_pred = torch.sum(X * loc, dim=-1)
+    expected_beta = mixMean(locs, weights)
+    y_pred = torch.sum(X * expected_beta, dim=-1)
     return y - y_pred
     
 def assembleNoiseInputs(y: torch.Tensor,
