c78a52ea1124c8b6059212edef2e871f834f8045
adkipnis <adkipnis@mailbox.org>
2025-04-14 15:44:24 +0200

    move functions to utils

--- utils.py
+++ utils.py
@@ -3,10 +3,15 @@ from typing import Tuple
 import torch
 
 
+def dInput(d_data: int, fx_type: str) -> int:
+    n_fx = 2 if fx_type == 'mfx' else 1
+    return 1 + n_fx * (1 + d_data)
+
 def getAlpha(noise_std: torch.Tensor) -> torch.Tensor:
     b, n  = noise_std.shape
     ns = torch.stack([torch.arange(n)] * b)
     return 2. + ns / 2.
+
 
 def moments2ig(loc: torch.Tensor, scale: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
     ''' reparameterize IG-distribution (from loc and scale to alpha and beta) '''
@@ -15,6 +20,10 @@ def moments2ig(loc: torch.Tensor, scale: torch.Tensor) -> Tuple[torch.Tensor, to
     beta = torch.mul(loc, alpha - 1.)
     return alpha, beta
 
+
+def causalMask(seq_len: int) -> torch.Tensor:
+    mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1)
+    return mask.masked_fill(mask == 1, float('-inf'))
 
 
 def symmetricMatrix2Vector(matrix: torch.Tensor) -> torch.Tensor:
@@ -48,16 +57,52 @@ def parseVariance(fixed: float) -> str:
     return noise
 
 
-def dsFilename(ds_type: str, d: int, n: int, fixed: float, size: int, part: int) -> Path:
-    ''' example: ffx-train-d=8-n=50-noise=fixed-10k-001.pt'''
-    noise = parseVariance(fixed)
+def dsFilename(fx_type: str, ds_type: str, d: int, n: int, size: int, part: int) -> Path:
+    ''' example: ffx-train-d=8-n=50-10k-001.pt'''
     s = parseSize(size)
     p = f'{part:03d}'
-    return Path('data', f'{ds_type}-train-d={d}-n={n}-noise={noise}-{s}-{p}.pt')
+    return Path('data', f'{fx_type}-{ds_type}-d={d}-n={n}-{s}-{p}.pt')
 
 
-def dsFilenameVal(ds_type: str, d: int, n: int, fixed: float, suffix: str = '') -> Path:
-    noise = parseVariance(fixed)
-    return Path('data', f'{ds_type}-val-d={d}-n={n}-noise={noise}{suffix}.pt')
+# def averageOverN(losses: torch.Tensor,
+#                  n: int,
+#                  b: int,
+#                  depths: torch.Tensor,
+#                  n_min: int = 0,
+#                  weigh: bool = False) -> torch.Tensor:
+#     ''' mask out first n_min losses per batch,
+#     then calculate weighted average over n with higher emphasis on later n'''
+#     # losses (b, n, d)
+#     if cfg.tol == 0:
+#         return losses.mean(dim=1)
+#     if n_min == 0:
+#         n_min = cfg.tol * depths.unsqueeze(1) # (b, 1)
+#     denominators = n - n_min # (b, 1)
+#     mask = torch.arange(n).expand(b, n) < n_min # (b, n)
+#     losses[mask] = 0.
+#     if weigh:
+#         weights = torch.arange(0, 1, 1/n) + 1/n
+#         weights = weights.sqrt().unsqueeze(0).unsqueeze(-1)
+#         losses = losses * weights
+#     losses = losses.sum(dim=1) / denominators
+#     return losses # (batch, d)
 
 
+# def assembleNoiseInputs(y: torch.Tensor, X: torch.Tensor,
+#                         outputs: Dict[str, torch.Tensor],
+#                         posterior_type: str) -> Tuple[torch.Tensor, torch.Tensor]:
+#     if posterior_type == "discrete":
+#         logits = outputs["ffx_logits"]
+#         loc = dprop_normal.mean(logits).detach()
+#         scale = dprop_normal.variance(logits, loc).detach().sqrt()
+#     elif posterior_type == "mixture":
+#         locs = outputs["ffx_loc"]
+#         scales = outputs["ffx_scale"]
+#         weights = outputs["ffx_weight"]
+#         loc = mprop.mean(locs, scales, weights).detach()
+#         scale = mprop.variance(locs, scales, weights, loc).sqrt().detach()
+#     else:
+#         raise ValueError(f"posterior type {posterior_type} not supported.")
+#     y_pred = torch.sum(X * loc, dim=-1).unsqueeze(-1)
+#     res = y - y_pred
+#     return res, scale
