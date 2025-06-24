b5985d61d4e6e03ae853c9677719a10052fcd50f
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:52:10 +0200

    todos

--- metabeta/evaluate.py
+++ metabeta/evaluate.py
@@ -16,3 +16,34 @@ plt.rcParams['figure.dpi'] = 300
 cmap = plt.get_cmap("tab10")
 colors = [cmap(i) for i in range(cmap.N)]
 
+# --- for given snapshot:
+# 1. evaluate performance (over batch):
+# - plot recovery  [✓]
+# - plot error bars [✓]
+# - add rmse + r  [✓]
+
+# 2. calibration
+# - coverage over different CI's [✓]
+# - plot calibration curve (nominal vs. empirical) [✓]
+# - SBC histogram [✓] 
+# - SBC shade 
+# - ECDF difference plot [✓] 
+
+# 3. visualize posterior (conditional on single dataset):
+# - plot posterior distribution with vline for true values [✓]
+# - (1) mixture [✓]
+# - (2) discrete [✓]
+# - (3) flow [✓]
+
+# 4. visualize posterior devolopment over n (conditional on single dataset):
+# - evaluate quantiles [✓]
+# - subset dataset and run model on each [✓]
+# - plot quantiles [✓]
+
+# 5. posterior predictive checks
+# - read up on theory
+
+# --- over snapshots:
+# 1. visualize training and testing NLLL [✓]
+# 2. evaluate [✓] and visualize KLD [✓]
+
