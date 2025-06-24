187893decdc24005fe4e7415ddc268f3ccccc6a9
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:38:14 +0200

    init coupling

---
+++ metabeta/models/normalizingflows/coupling.py
@@ -0,0 +1,8 @@
+from typing import List, Tuple
+import torch
+from torch import nn
+from metabeta.models.normalizingflows.linear import Transform, Permute, LU, ActNorm
+from metabeta.models.normalizingflows.couplingtransforms import Affine, RationalQuadratic
+from metabeta.models.normalizingflows.distributions import DiagGaussian, DiagStudent, DiagUniform
+
+
