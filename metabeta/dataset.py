069ba35ca360b1b7820afcf959daad5d13d9eea3
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:25:59 +0200

    base

--- metabeta/dataset.py
+++ metabeta/dataset.py
@@ -14,3 +14,42 @@ def padTensor(tensor: torch.Tensor, shape: tuple, value=0) -> torch.Tensor:
     return F.pad(tensor, padding, value=value)
 
 
+class LMDataset(Dataset):
+    def __init__(self,
+                 data: List[dict],
+                 info: dict,
+                 permute: bool = True,
+                 ) -> None:
+        self.info = info
+        self.permute = permute
+        self.n_max = self.info['max_n']
+        self.d_max = self.info['max_d']
+        self.m_max = self.info['max_m']
+        self.q_max = self.info['max_q']
+        self.len_max = self.n_max * self.m_max
+        
+        self.fx_type = 'mfx' if 'rfx' in data[0] else 'ffx'
+        self.data = [self.preprocess(item) for item in data]
+
+    def __len__(self) -> int:
+       return len(self.data)
+   
+    def __getitem__(self, idx) -> dict:
+        out = self.data[idx]
+        return out
+
+    def getPermutation(self, d: int):
+        idx = torch.randperm(d-1) + 1
+        zero = torch.zeros((1,), dtype=idx.dtype)
+        idx = torch.cat([zero, idx])
+        return idx
+
+    def preprocess(self, item: dict) -> dict:
+       if self.fx_type == 'ffx':
+           return self.preprocessFfx(item)
+       elif self.fx_type == 'mfx':
+           return self.preprocessMfx(item)
+       else:
+           raise ValueError(f'Unsupported fx type {self.fx_type} for preprocessing')
+
+
