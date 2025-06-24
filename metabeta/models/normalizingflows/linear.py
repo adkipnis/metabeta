6105d49861e6047144d86e48e1807a360f8491bb
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:37:29 +0200

    actnorm

--- metabeta/models/normalizingflows/linear.py
+++ metabeta/models/normalizingflows/linear.py
@@ -24,3 +24,32 @@ class Transform(nn.Module):
         return mask
 
 
+class ActNorm(Transform):
+    # adapted from https://github.com/bayesflow/bayesflow to handle masking
+    def __init__(self, target_dim: int):
+        super().__init__()
+        self.scale = nn.Parameter(torch.ones((target_dim,)))
+        self.bias = nn.Parameter(torch.zeros((target_dim,)))
+
+    def forward(self, x, condition=None, mask=None):
+        s, t = self.scale.expand_as(x), self.bias.expand_as(x)
+        if mask is not None:
+            t = t * mask
+        z = s * x + t
+        log_det = s.abs().log()
+        if mask is not None:
+            log_det = log_det * mask
+        return z, log_det.sum(-1), mask
+
+    def inverse(self, z, condition=None, mask=None):
+        s, t = self.scale.expand_as(z), self.bias.expand_as(z)
+        # s, t = self.scale, self.bias
+        if mask is not None:
+            t = t * mask
+        x = (z - t) / s
+        log_det = -s.abs().log()
+        if mask is not None:
+            log_det = log_det * mask
+        return x, log_det.sum(-1), mask
+
+
