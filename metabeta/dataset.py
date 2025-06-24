5fd64f388b43b0d7b73bf71ac7dff5504bade891
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:25:12 +0200

    pad tensor

--- metabeta/dataset.py
+++ metabeta/dataset.py
@@ -4,3 +4,13 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset
 
 
+def padTensor(tensor: torch.Tensor, shape: tuple, value=0) -> torch.Tensor:
+    assert len(tensor.shape) == len(shape),\
+        "Input tensor and target shape must have the same number of dimensions"
+    pad_size = [max(s - t, 0) for s, t in zip(reversed(shape), reversed(tensor.shape))]
+    padding = []
+    for p in pad_size:
+        padding.extend([0, p])  # Pad at the end of each dimension
+    return F.pad(tensor, padding, value=value)
+
+
