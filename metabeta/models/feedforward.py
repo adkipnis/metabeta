74b7354e4221ee77af95bf9c48b8676bc2da5912
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:32:45 +0200

    slp block

--- metabeta/models/feedforward.py
+++ metabeta/models/feedforward.py
@@ -24,3 +24,34 @@ def initializer(method: str, distribution: str):
     return initialize
 
 
+class SLP(nn.Module):
+    def __init__(self,
+                 d_input: int,
+                 d_output: int,
+                 activation: str = 'Mish',
+                 dropout: float = 0.01,
+                 norm: str | None = None,
+                 skip: bool = False,
+                 **kwargs
+                 ):
+        super().__init__()
+        layer = [nn.Linear(d_input, d_output)]
+        if norm == 'batch':
+            layer += [nn.BatchNorm1d(d_output, eps=1e-3)]
+        elif norm == 'layer':
+            layer += [nn.LayerNorm(d_output, eps=1e-3)]
+        layer += [eval(f'nn.{activation}()')]
+        layer += [nn.Dropout(dropout)]
+        self.net = nn.Sequential(*layer)
+        # optional skip connection
+        self.skip = skip
+        if self.skip:
+            self.shortcut = nn.Linear(d_input, d_output)
+
+    def forward(self, x):
+        h = self.net(x)
+        if self.skip:
+            h += self.shortcut(x)
+        return h
+
+
