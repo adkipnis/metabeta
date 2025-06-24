20394f59866748bb56610f439b04713909bb03ef
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:23:57 +0200

    helpers

--- metabeta/generate.py
+++ metabeta/generate.py
@@ -10,3 +10,32 @@ from metabeta.utils import dsFilename
 from metabeta.markov import fitFFX, fitMFX
 
 
+# -----------------------------------------------------------------------------
+# helpers
+def sampleInt(batch_size: int | tuple, min_val: int, max_val: int) -> torch.Tensor:
+    ''' sample batch from discrete uniform (include the max_val as possible value) '''
+    shape = (batch_size,) if isinstance(batch_size, int) else batch_size
+    return torch.randint(min_val, max_val+1, shape)
+
+def sampleHN(shape: tuple, scale: float, max_val: float = float('inf'),) -> torch.Tensor:
+    ''' sample batch from halfnormal, optionally clamp '''
+    val = D.HalfNormal(scale).sample(shape)
+    if max_val < float('inf'):
+        val = val.clamp(max=max_val) # type: ignore
+    return val # type: ignore
+
+def sampleIG(shape: tuple, alpha: float, beta: float, max_val: float = float('inf')) -> torch.Tensor:
+    ''' sample batch from inverse gamma (and apply sqrt), optionally clamp '''
+    val = D.InverseGamma(alpha, beta).sample(shape).sqrt() # type: ignore
+    if max_val < float('inf'):
+        val = val.clamp(max=max_val) # type: ignore
+    return val # type: ignore
+
+def constrain(a: torch.Tensor, b: torch.Tensor, max_ratio: float, min_val: int):
+    ''' constrain b such that a * max_ratio >= b (element-wise) '''
+    bounds = a * max_ratio
+    indices = bounds < b
+    b[indices] = bounds[indices].floor().to(b.dtype).clamp(min=min_val)
+    return a, b
+
+
