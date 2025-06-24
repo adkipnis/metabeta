6aa1f495db985982b244468b6b8023650716781e
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:33:46 +0200

    base class

--- metabeta/models/summarizers.py
+++ metabeta/models/summarizers.py
@@ -5,3 +5,15 @@ from torch.nn import MultiheadAttention, TransformerEncoderLayer
 from metabeta.models.feedforward import MLP
 
 
+# --------------------------------------------------------------------------
+# Base Class
+class Summarizer(nn.Module):
+    ''' takes embedded batch of sequential data x (batch, seq_len, d_model)
+    and summarizes the sequences to h (batch, d_output) '''
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor | None = None) -> torch.Tensor:
+        raise NotImplementedError
+
+
