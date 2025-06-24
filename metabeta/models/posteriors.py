b90a2f753156c17a85c77fca59fe04f5588cd491
adkipnis <adkipnis@mailbox.org>
2025-06-24 13:35:32 +0200

    base class

--- metabeta/models/posteriors.py
+++ metabeta/models/posteriors.py
@@ -48,3 +48,80 @@ class GeneralizedProposer(nn.Module):
 
 
 
+# -----------------------------------------------------------------------------
+# base class
+class Posterior(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.other = None
+
+    def propose(self, s: torch.Tensor) -> torch.Tensor:
+        ''' x (batch, d, emb) -> h (batch, ...) '''
+        h = self.prop(s) # type: ignore
+        return h
+
+    def mean(self, h: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+    def variance(self, h: torch.Tensor, mean: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+    def getLocScale(self, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        loc = self.mean(h)
+        scale = self.variance(h, loc).sqrt()
+        return loc, scale
+    
+    def getCDF(self, h: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        # returns x, cdf (batch, d, n_samples)
+        raise NotImplementedError
+
+    def getQuantiles(self, h: torch.Tensor, roots: list = [.05, .50, .95]) -> torch.Tensor:
+        x, cdf = self.getCDF(h)
+        cdf = cdf.contiguous()
+        b, d, s, r = *cdf.shape, len(roots)
+        roots_ = torch.tensor(roots).view(1, 1, -1).expand((b, d, r)).contiguous()
+        indices = torch.searchsorted(cdf, roots_).clamp(max=s-1)
+        quantiles = x.gather(dim=-1, index=indices)
+        return quantiles
+
+    def logProb(self, s: torch.Tensor, values: torch.Tensor, **kwargs):
+        h = self.propose(s)
+        return self._logProb(h, values)
+
+    def _logProb(self, h: torch.Tensor, values: torch.Tensor):
+        # from hidden proposal representations
+        raise NotImplementedError
+
+    def loss(self, h: torch.Tensor, targets: torch.Tensor, **kwargs) -> torch.Tensor:
+        ''' calculate losses for each target dim, mask out padded target dims,
+        aggregate over targets and average over batch '''
+        raise NotImplementedError
+
+    def forward(self, s: torch.Tensor, targets: torch.Tensor,
+                **kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
+        h = self.propose(s)
+        loss = self.loss(h, targets)
+        return loss, h
+
+    def plot(self, h: torch.Tensor, target: torch.Tensor, names: List[str],
+             batch_idx: int = 0, **kwargs):
+        raise NotImplementedError
+
+# -----------------------------------------------------------------------------
+# point posterior
+class PointPosterior(Posterior):
+    def __init__(self, d_model: int, d_output: int):
+        super().__init__()
+        self.prop = nn.Linear(d_model, d_output)
+
+    def mean(self, h: torch.Tensor):
+        return h
+
+    def variance(self, h: torch.Tensor, mean: torch.Tensor):
+        return torch.zeros_like(h)
+
+    def loss(self, h: torch.Tensor, targets: torch.Tensor, **kwargs):
+        losses = mse(h, targets) 
+        return maskLoss(losses, targets)
+
+
