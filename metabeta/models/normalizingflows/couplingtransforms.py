d81fde961634fb80dac78bbdc922dcd8a3f24c86
adkipnis <adkipnis@mailbox.org>
2025-10-07 17:23:01 +0200

    init

---
+++ metabeta/models/normalizingflows/couplingtransforms.py
@@ -0,0 +1,8 @@
+from typing import List, Tuple, Dict
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+from metabeta.models.feedforward import MLP, ResidualNet
+
+
