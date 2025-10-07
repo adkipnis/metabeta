8c21ebc166f4d868bf73f4fb89cc50124bb3828a
adkipnis <adkipnis@mailbox.org>
2025-10-07 17:45:31 +0200

    cfg

--- metabeta/train.py
+++ metabeta/train.py
@@ -19,3 +19,56 @@ from metabeta import plot
 
 
 def setup() -> argparse.Namespace:
+    ''' Parse command line arguments. '''
+    parser = argparse.ArgumentParser()
+
+    # misc
+    parser.add_argument('--m_tag', type=str, default='r', help='Suffix for model ID (default = '')')
+    parser.add_argument('-s', '--seed', type=int, default=42, help='Model seed')
+    parser.add_argument('--device', type=str, default='mps', help='Device to use [cpu, cuda, mps]')
+    parser.add_argument('--cores', type=int, default=8, help='Nubmer of processor cores to use (default = 8)')
+
+    # data
+    parser.add_argument('--d_tag', type=str, default='r', help='Suffix for data ID (default = '')')
+    parser.add_argument('--varied', action='store_true', help='Use data with variable d/q (default = False)')
+    parser.add_argument('--semi', action='store_true', help='Use semi-synthetic data (default = True)')
+    parser.add_argument('-t', '--fx_type', type=str, default='mfx', help='Type of dataset [ffx, mfx] (default = ffx)')
+    parser.add_argument('-d', type=int, default=3, help='Number of fixed effects (with bias, default = 5)')
+    parser.add_argument('-q', type=int, default=1, help='Number of random effects (with bias, default = 1)')
+    parser.add_argument('-m', type=int, default=30, help='Maximum number of groups (default = 30).')
+    parser.add_argument('-n', type=int, default=70, help='Maximum number of samples per group (default = 70).')
+    parser.add_argument('--permute', action='store_true', help='Permute slope variables for uniform learning across heads (default = True)')
+
+    # training
+    parser.add_argument('--patience', type=int, default=25, help='Maximum number of iterations without improvement before Early Stopping (default = 5)')
+    parser.add_argument('--bs-train', type=int, default=4096, help='macro batch size per training partition (default = 4096).')
+    parser.add_argument('--bs-val', type=int, default=256, help='macro batch size for validation partition (default = 256).')
+    parser.add_argument('--bs-mini', type=int, default=32, help='mini batch size (default = 32)')
+    parser.add_argument('--lr', type=float, default=5e-4, help='Learning rate (Adam, default = 5e-4)')
+    parser.add_argument('--standardize', action='store_false', help='Standardize inputs (default = True)')
+    parser.add_argument('-p', '--preload', type=int, default=0, help='Preload model from iteration #p')
+    parser.add_argument('-i', '--iterations', type=int, default=10, help='Maximum number of iterations to train (default = 10)')
+
+    # summary network
+    parser.add_argument('--sum_type', type=str, default='set-transformer', help='Summarizer architecture [set-transformer, dual-transformer] (default = set-transformer)')
+    parser.add_argument('--sum_blocks', type=int, default=3, help='Number of blocks in summarizer (default = 4)')
+    parser.add_argument('--sum_d', type=int, default=128, help='Model dimension (default = 128)')
+    parser.add_argument('--sum_ff', type=int, default=128, help='Feedforward dimension (default = 256)')
+    parser.add_argument('--sum_depth', type=int, default=1, help='Feedforward layers (default = 1)')
+    parser.add_argument('--sum_out', type=int, default=64, help='Summary dimension (default = 64)')
+    parser.add_argument('--sum_heads', type=int, default=8, help='Number of heads (poolformer, default = 8)')    
+    parser.add_argument('--sum_dropout', type=float, default=0.01, help='Dropout rate (default = 0.01)')
+    parser.add_argument('--sum_act', type=str, default='GELU', help='Activation funtction [anything implemented in torch.nn] (default = GELU)')
+
+    # posterior network
+    parser.add_argument('--post_type', type=str, default='affine', help='Normalizing flow architecture [affine, spline] (default = affine)')
+    parser.add_argument('--flows', type=int, default=3, help='Number of normalizing flow blocks (default = 3)')
+    parser.add_argument('--post_ff', type=int, default=128, help='Feedforward dimension (default = 128)')
+    parser.add_argument('--post_depth', type=int, default=3, help='Feedforward layers (default = 3)')
+    parser.add_argument('--post_dropout', type=float, default=0.01, help='Dropout rate (default = 0.01)')
+    parser.add_argument('--post_act', type=str, default='ReLU', help='Activation funtction [anything implemented in torch.nn] (default = GELU)')
+
+    return parser.parse_args()
+
+
+# -----------------------------------------------------------------------------
