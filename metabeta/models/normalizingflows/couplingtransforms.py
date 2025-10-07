6ca9acdb7aed2d3caf81e4af2a8932fec95d5ebc
adkipnis <adkipnis@mailbox.org>
2025-10-07 17:23:15 +0200

    base class

--- metabeta/models/normalizingflows/couplingtransforms.py
+++ metabeta/models/normalizingflows/couplingtransforms.py
@@ -6,3 +6,27 @@ from torch.nn import functional as F
 from metabeta.models.feedforward import MLP, ResidualNet
 
 
+class CouplingTransform(nn.Module):
+    def propose(
+        self, x1: torch.Tensor, condition: torch.Tensor | None = None
+    ) -> Dict[str, torch.Tensor]:
+        raise NotImplementedError
+
+    def forward(
+        self,
+        x2: torch.Tensor,
+        params: Dict[str, torch.Tensor],
+        mask2: torch.Tensor | None = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor | None]:
+        raise NotImplementedError
+
+    def inverse(
+        self,
+        z2: torch.Tensor,
+        params: Dict[str, torch.Tensor],
+        mask2: torch.Tensor | None = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor | None]:
+        raise NotImplementedError
+
+
+class Affine(CouplingTransform):
