34c5c12c2a6280c932c93fc40780b1750ac96aec
adkipnis <adkipnis@mailbox.org>
2025-10-07 17:26:01 +0200

    defaults

--- metabeta/models/approximators.py
+++ metabeta/models/approximators.py
@@ -21,3 +21,29 @@ from metabeta import plot
 mse = nn.MSELoss()
 
 summary_defaults = {
+    "type": "set-transformer",
+    "d_model": 64,
+    "n_blocks": 3,
+    "d_ff": 128,
+    "depth": 1,
+    "d_output": 64,
+    "n_heads": 8,
+    "dropout": 0.01,
+    "activation": "GELU",
+}
+
+posterior_defaults = {
+    "type": "flow-affine",
+    "flows": 3,
+    "d_ff": 128,
+    "depth": 3,
+    "dropout": 0.01,
+    "activation": "ReLU",
+}
+
+model_defaults = {"type": "mfx", "seed": 42, "d": 5, "q": 2, "tag": ""}
+
+# -----------------------------------------------------------------------------
+
+
+# base approximator
