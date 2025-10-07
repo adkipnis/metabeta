f2d06002713839965f2c224a02c41564fb670d41
adkipnis <adkipnis@mailbox.org>
2025-10-07 17:21:07 +0200

    base class

--- metabeta/models/normalizingflows/linear.py
+++ metabeta/models/normalizingflows/linear.py
@@ -5,3 +5,25 @@ from torch.nn import functional as F
 from typing import Tuple
 
 
+class Transform(nn.Module):
+    def forward(
+        self,
+        x: torch.Tensor,
+        condition: torch.Tensor | None = None,
+        mask: torch.Tensor | None = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]:
+        raise NotImplementedError
+
+    def inverse(
+        self,
+        z: torch.Tensor,
+        condition: torch.Tensor | None = None,
+        mask: torch.Tensor | None = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]:
+        raise NotImplementedError
+
+    def forwardMask(self, mask: torch.Tensor):
+        return mask
+
+
+class ActNorm(Transform):
