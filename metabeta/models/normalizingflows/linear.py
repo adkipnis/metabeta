84fe9b894f86d17daebfcc20d0dcd23a8f33f835
adkipnis <adkipnis@mailbox.org>
2025-10-07 17:21:36 +0200

    permute

--- metabeta/models/normalizingflows/linear.py
+++ metabeta/models/normalizingflows/linear.py
@@ -56,3 +56,57 @@ class ActNorm(Transform):
 
 
 class Permute(Transform):
+    # adapted from https://github.com/bayesflow/bayesflow to handle masking
+    def __init__(self, target_dim: int, mode: str = "shuffle"):
+        super().__init__()
+        self.target_dim = target_dim
+        assert mode in ["shuffle", "swap"], "unkown mode selected"
+        self.mode = mode
+        self.pivot = target_dim // 2
+        self.inv_pivot = target_dim - self.pivot
+        if self.mode == "shuffle":
+            self.perm = torch.randperm(target_dim)
+            self.inv_perm = self.perm.argsort()
+
+    def forward(self, x, condition=None, mask=None):
+        if self.mode == "shuffle":
+            x = x[..., self.perm]
+            if mask is not None:
+                mask = mask[..., self.perm]
+        elif self.mode == "swap":
+            x1, x2 = x[..., : self.pivot], x[..., self.pivot :]
+            x = torch.cat([x2, x1], dim=-1)
+            if mask is not None:
+                mask1, mask2 = mask[..., : self.pivot], mask[..., self.pivot :]
+                mask = torch.cat([mask2, mask1], dim=-1)
+        else:
+            raise ValueError
+        log_det = torch.zeros(x.shape[:-1], device=x.device)
+        return x, log_det, mask
+
+    def forwardMask(self, mask):
+        if self.mode == "shuffle":
+            mask = mask[..., self.perm]
+        elif self.mode == "swap":
+            mask1, mask2 = mask[..., : self.pivot], mask[..., self.pivot :]
+            mask = torch.cat([mask2, mask1], dim=-1)
+        return mask
+
+    def inverse(self, z, condition=None, mask=None):
+        if self.mode == "shuffle":
+            z = z[..., self.inv_perm]
+            if mask is not None:
+                mask = mask[..., self.inv_perm]
+        elif self.mode == "swap":
+            z1, z2 = z[..., : self.inv_pivot], z[..., self.inv_pivot :]
+            z = torch.cat([z2, z1], dim=-1)
+            if mask is not None:
+                mask1, mask2 = mask[..., : self.inv_pivot], mask[..., self.inv_pivot :]
+                mask = torch.cat([mask2, mask1], dim=-1)
+        else:
+            raise ValueError
+        log_det = torch.zeros(z.shape[:-1], device=z.device)
+        return z, log_det, mask
+
+
+class LU(Transform):
