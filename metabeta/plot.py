71e28e715a87859ec7be9fddc038335b56035148
adkipnis <adkipnis@mailbox.org>
2025-10-07 17:28:06 +0200

    recovery

--- metabeta/plot.py
+++ metabeta/plot.py
@@ -72,3 +72,87 @@ def posterior(
 
 
 def recovery(
+    targets: torch.Tensor,
+    names: list[str],
+    means: torch.Tensor,
+    color: str = "darkgreen",
+    alpha: float = 0.3,
+    return_stats: bool = True,
+) -> None | tuple[float, float]:
+    """plot true targets against posterior means for entire batch"""
+    assert means.shape[-1] == len(names), "shape mismatch"
+    D = len(names)
+    if targets.dim() == 3:
+        targets = targets.view(-1, D)
+        means = means.view(-1, D)
+    mask = targets != 0.0
+    w = int(torch.tensor(D).sqrt().ceil())
+
+    # init figure
+    fig, axs = plt.subplots(figsize=(8 * w, 7 * w), ncols=w, nrows=w)
+    if w > 1:
+        axs = axs.flatten()
+
+    # init stats
+    RMSE = 0.0
+    R = 0.0
+    denom = D
+
+    # make subplots
+    for i in range(D):
+        ax = axs[i] if w > 1 else axs
+        mask_i = mask[..., i]
+        targets_i = targets[mask_i, i]
+        mean_i = means[mask_i, i].detach()
+
+        # skip empty target
+        if mask_i.sum() == 0:
+            axs[i].set_visible(False)
+            denom -= 1
+            continue
+
+        # compute stats
+        r = float(pearsonr(targets_i, mean_i)[0])  # type: ignore
+        R += r
+        bias = (targets_i - mean_i).mean()
+        rmse = mse(targets_i, mean_i).sqrt()
+        RMSE += rmse
+
+        # subplot
+        ax.set_axisbelow(True)
+        ax.grid(True)
+        min_val = min(mean_i.min(), targets_i.min()).floor()
+        max_val = max(mean_i.max(), targets_i.max()).ceil()
+        ax.plot(
+            [min_val, max_val], [min_val, max_val], "--", lw=2, zorder=1, color="grey"
+        )  # diagline
+        ax.scatter(targets_i, mean_i, alpha=alpha, color=color, label=names[i])
+        ax.text(
+            0.75,
+            0.1,
+            f"r = {r:.3f}\nBias = {bias.item():.3f}\nRMSE = {rmse.item():.3f}",
+            transform=ax.transAxes,
+            ha="center",
+            va="bottom",
+            fontsize=16,
+            bbox=dict(
+                boxstyle="round",
+                facecolor=(1, 1, 1, 0.7),
+                edgecolor=(0, 0, 0, alpha),
+            ),
+        )
+        ax.set_xlabel("true", fontsize=20)
+        ax.set_ylabel("estimated", fontsize=20)
+        ax.legend()
+
+    # skip remaining empty subplots
+    for i in range(D, w * w):
+        axs[i].set_visible(False)
+    fig.tight_layout()
+
+    # optionally return average statistics
+    if return_stats:
+        return RMSE / denom, R / denom
+
+
+def _recoveryGrouped(
