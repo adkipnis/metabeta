0e91be7cf19be53818b83869af4805dec59a9bc4
adkipnis <adkipnis@mailbox.org>
2025-11-13 20:30:19 +0100

    init alternative mcmc procedure using bambi

---
+++ metabeta/data/markov_bambi.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import torch
+import bambi as bmb
+from bambi import Prior
+import arviz as az
+import time
+
+from torch import distributions as D
+import matplotlib.pyplot as plt
