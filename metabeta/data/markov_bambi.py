8a0d6c7afd5b7852683673a495101de4dfb89994
adkipnis <adkipnis@mailbox.org>
2025-11-13 20:30:36 +0100

    make pandas df from ds dict

--- metabeta/data/markov_bambi.py
+++ metabeta/data/markov_bambi.py
@@ -7,3 +7,14 @@ import time
 
 from torch import distributions as D
 import matplotlib.pyplot as plt
+
+def pandify(ds: dict[str, torch.Tensor]) -> pd.DataFrame:
+    n, d = int(ds['n']), int(ds['d'])
+    df = pd.DataFrame(index=range(n))
+    df['i'] = ds['groups'].numpy()
+    df['y'] = ds['y'].unsqueeze(-1).numpy()
+    for j in range(1, d):
+        df[f'x{j}'] = ds['X'][..., j].numpy()
+    return df
+
+
