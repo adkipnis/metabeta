b37c331f1c3cda2b2f7a624015979e9d424bab69
adkipnis <adkipnis@mailbox.org>
2025-11-13 20:30:51 +0100

    create autoformula for bambi

--- metabeta/data/markov_bambi.py
+++ metabeta/data/markov_bambi.py
@@ -18,3 +18,13 @@ def pandify(ds: dict[str, torch.Tensor]) -> pd.DataFrame:
     return df
 
 
+def formulate(ds: dict[str, torch.Tensor]) -> str:
+    d, q = int(ds['d']), int(ds['q'])
+    fixed = ' + '.join(f'x{j}' for j in range(1, d))
+    random = ' + '.join(f'x{j}' for j in range(1, q))
+    if not random:
+        random = '1'
+    out = f'y ~ 1 + {fixed} + ({random} | i)'
+    return out
+
+
