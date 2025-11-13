b70da2696921b3f1bf1a8b90cf4056de87eda78c
adkipnis <adkipnis@mailbox.org>
2025-11-13 20:36:41 +0100

    simplified generate script using single.py instead of tasks.py

---
+++ metabeta/data/generate.py
@@ -0,0 +1,254 @@
+import os
+from sys import exit
+from pathlib import Path
+from tqdm import tqdm
+import argparse
+import numpy as np
+import torch
+from torch import distributions as D
+
+from metabeta.utils import dsFilename, padTensor
+from metabeta.data.single import Prior, Design, Generator
+from metabeta.data.markov import fitMCMC
+# from metabeta.data.csv import RealDataset
+
+
+# -----------------------------------------------------------------------------
+# config
+def setup() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Generate hierarchical datasets.")
+    parser.add_argument("--bs_train", type=int, default=4096, help="batch size per training partition (default = 4,096).")
+    parser.add_argument("--bs_val", type=int, default=256, help="batch size for validation partition (default = 256).")
+    parser.add_argument("--bs_test", type=int, default=32, help="batch size per testing partition (default = 128).")
+    parser.add_argument("--bs_load", type=int, default=32, help="Batch size when loading (for grouping n, default = 32)")
+    parser.add_argument("--min_m", type=int, default=5, help="Minimum number of groups (default = 5).")
+    parser.add_argument("--max_m", type=int, default=30, help="Maximum number of groups (default = 30).")
+    parser.add_argument("--min_n", type=int, default=10, help="Minimum number of samples per group (default = 10).")
+    parser.add_argument("--max_n", type=int, default=70, help="Maximum number of samples per group (default = 70).")
+    parser.add_argument("--max_d", type=int, default=2, help="Maximum number of fixed effects (intercept + slopes) to draw per linear model (default = 12).")
+    parser.add_argument("--max_q", type=int, default=1, help="Maximum number of random effects (intercept + slopes) to draw per linear model (default = 4).")
+    parser.add_argument("--d_tag", type=str, default="hierarchical", help='Suffix for model ID (default = "")')
+    parser.add_argument("--toy", action="store_false", help="Generate toy data (default = False)")
+    parser.add_argument("--semi", action="store_true", help="Generate semi-synthetic data (default = False)")
+    # parser.add_argument("--varied", action="store_true", help="variable d and q (default = False)")
+    parser.add_argument("-b", "--begin", type=int, default=-1, help="Begin with iteration number #b (default = 0).")
+    parser.add_argument("-i", "--iterations", type=int, default=10, help="Number of dataset partitions to generate (default = 100, 0 only generates validation dataset).")
+    return parser.parse_args()
+
+# -----------------------------------------------------------------------------
+# helpers
+def getFileName(ds_type: str, part: int) -> Path:
+    if "train" in ds_type:
+        size = cfg.bs_train
+        b = cfg.bs_load
+    elif "val" in ds_type:
+        size = cfg.bs_val
+        b = 1
+    elif "test" in ds_type:
+        size = cfg.bs_test
+        b = 1
+    else:
+        raise ValueError
+    fn = dsFilename(
+        fx_type="mfx",
+        ds_type=ds_type,
+        b=b,
+        m=cfg.max_m,
+        n=cfg.max_n,
+        d=cfg.max_d,
+        q=cfg.max_q,
+        size=size,
+        part=part,
+        # varied=cfg.varied,
+        tag=cfg.d_tag,
+        outside=True,
+    )
+    return fn
+
+
+def sampleInt(batch_size: int | tuple, min_val: int, max_val: int) -> torch.Tensor:
+    """sample batch from discrete uniform
+    (include the max_val as possible value)"""
+    shape = (batch_size,) if isinstance(batch_size, int) else batch_size
+    return torch.randint(min_val, max_val + 1, shape)
+
+
+def sampleIntBatched(
+    min_val: int | torch.Tensor, max_val: int | torch.Tensor
+) -> torch.Tensor:
+    """batched version of the above with varying bounds"""
+    if isinstance(min_val, int):
+        min_val = torch.tensor([min_val])
+    if isinstance(max_val, int):
+        max_val = torch.tensor([max_val])
+    n = max(len(min_val), len(max_val))
+    samples = min_val + torch.floor((max_val + 1 - min_val) * torch.rand(n)).long()
+    return samples
+
+
+def sampleHN(shape: tuple, scale: float, max_val: float = float("inf")) -> torch.Tensor:
+    """sample batch from halfnormal, optionally clamp"""
+    val = D.HalfNormal(scale).sample(shape)
+    if max_val < float("inf"):
+        val = val.clamp(max=max_val) # type: ignore
+    return val # type: ignore
+
+
+# -----------------------------------------------------------------------------
+# aggregation
+
+def maxShapes(data: list[dict[str, torch.Tensor]]) -> dict[str, tuple[int, ...]]:
+    ''' get the maximal shapes over the whole list for per key '''
+    max_shapes = {}
+    for entry in data:
+        for key, tensor in entry.items():
+            if not isinstance(tensor, torch.Tensor):
+                raise ValueError("expected all entries to be tensors")
+            shape = tuple(tensor.shape)
+            if key not in max_shapes:
+                max_shapes[key] = shape
+            else:
+                # Expand max_shapes[key] tuple elementwise to max dimension
+                max_shapes[key] = tuple(
+                    max(old_dim, new_dim)
+                    for old_dim, new_dim in zip(max_shapes[key], shape)
+                )
+    return max_shapes
+
+
+def aggregate(data: list[dict[str, torch.Tensor]]) -> dict[str, torch.Tensor]:
+    ''' finds out maximal dimensions for each key in list of tensor-dicts
+        collates all tensor-dicts to single tensor-dicts with padded tensors '''
+    max_shapes = maxShapes(data)
+    batch_size = len(data)
+    ds = {
+        k: torch.empty(batch_size, *shape, dtype=data[0][k].dtype)
+        for k, shape in max_shapes.items()
+    }
+    for i, item in enumerate(data):
+        for k in ds.keys():
+            shape = max_shapes[k]
+            ds[k][i] = padTensor(item[k], shape)
+    return ds
+
+
+# -----------------------------------------------------------------------------
+# generators
+
+def generate(
+    batch_size: int,
+    seed: int,
+    mcmc: bool = False,
+    bs_load: int = 1,
+) -> list[dict[str, torch.Tensor]]:
+    """Generate a [batch_size] list of mixed effects datasets
+    with varying n, m, d, q."""
+    
+    # init
+    torch.manual_seed(seed)
+    data = []
+    iterator = tqdm(range(batch_size))
+    if not mcmc:
+        iterator.set_description(f"{part:02d}/{cfg.iterations:02d}")
+
+    # presample sizes
+    d = sampleInt(batch_size, cfg.max_d, cfg.max_d)
+    q = sampleInt(batch_size, cfg.max_q, cfg.max_q)
+    m = sampleInt(batch_size // bs_load, cfg.min_m, cfg.max_m).repeat_interleave(
+        bs_load
+    )
+    n = sampleInt((batch_size, cfg.max_m), cfg.min_n, cfg.max_n)
+    d, m, n, q = d.tolist(), m.tolist(), n.tolist(), q.tolist()
+    
+    # presample priors
+    nu_ffx = D.Uniform(-20, 20).sample((batch_size, cfg.max_d))
+    tau_ffx0 = D.Uniform(1, 30).sample((batch_size, 1))
+    tau_ffx1 = D.Uniform(1, 20).sample((batch_size, cfg.max_d - 1))
+    tau_ffx = torch.cat([tau_ffx0, tau_ffx1], dim=-1)
+    tau_rfx = D.Uniform(1, 20).sample((batch_size, cfg.max_d))
+    tau_eps = D.Uniform(1, 20).sample((batch_size,))
+    if cfg.toy:  # smaller ranges
+        nu_ffx = sampleInt((batch_size, cfg.max_d), 0, 0).float()
+        tau_ffx0 = D.Uniform(1, 10).sample((batch_size, 1))
+        tau_ffx1 = D.Uniform(1, 5).sample((batch_size, cfg.max_d - 1))
+        tau_ffx = torch.cat([tau_ffx0, tau_ffx1], dim=-1)
+        tau_rfx = D.Uniform(1, 5).sample((batch_size, cfg.max_d))
+        tau_eps = D.Uniform(1, 5).sample((batch_size,))
+
+    # sample datasets
+    for i in iterator:
+        ds = None
+        attempts = 0
+        okay = False
+        d_i, q_i, n_i = d[i], q[i], n[i][: m[i]]
+        prior = Prior(nu_ffx[i, :d_i],
+                      tau_ffx[i, :d_i],
+                      tau_eps[i],
+                      tau_rfx[i, :q_i])
+        design = Design()
+        while not okay:
+            ds = Generator(prior, design, n_i).sample()
+            okay = ds["okay"]
+            if not okay:
+                attempts += 1
+                if attempts > 20:
+                    okay = True
+                    print(f"\nWarning: outlier ds with sd(y)={ds['y'].std(0):.2f}")
+        data += [ds]
+    
+    # optionally fit mcmc
+    if mcmc:
+        print('Starting MCMC sampling...')
+        for i, ds in enumerate(tqdm(data)):
+            mcmc_results = fitMCMC(ds, seed=i)
+            ds.update(mcmc_results)
+    return data
+
+
+
+# =============================================================================
+if __name__ == "__main__":
+    
+    # init outout directory
+    os.makedirs(Path("..", "outputs", "data"), exist_ok=True)
+    
+    # setup config
+    cfg = setup()
+    assert cfg.bs_train % cfg.bs_load == 0, (
+        "storage batch size must be divisible by loading batch size"
+    )
+    part = cfg.begin
+    raw = None
+
+    # generate test dataset
+    if cfg.begin == -1:
+        print("Generating test set...")
+        ds_test = generate(cfg.bs_test, part, mcmc=True)
+        ds_test = aggregate(ds_test)
+        fn = getFileName("test", -1)
+        np.savez_compressed(fn, **ds_test, allow_pickle=True)
+        print(f"\nSaved test set to {fn}")
+        exit()
+
+    # generate validation dataset
+    if cfg.begin == 0:
+        print("Generating validation set...")
+        ds_val = generate(cfg.bs_val, part, mcmc=False)
+        ds_val = aggregate(ds_val)
+        fn = getFileName("val", 0)
+        np.savez_compressed(fn, **ds_val, allow_pickle=True)
+        print(f"\nSaved validation set to {fn}")
+        cfg.begin += 1
+
+    # potentially stop after that
+    if cfg.iterations == 0:
+        exit()
+
+    # generate training datasets
+    print(f"Generating {cfg.iterations} training partitions of {cfg.bs_train} datasets each...")
+    for part in range(cfg.begin, cfg.iterations + 1):
+        ds_train = generate(cfg.bs_train, part, bs_load=cfg.bs_load)
+        ds_train = aggregate(ds_train)
+        fn = getFileName("train", part)
+        np.savez_compressed(fn, **ds_train, allow_pickle=True)
+        print(f"Saved training set to {fn}")
