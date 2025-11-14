1e114354ee0bc6fafeced9f662e6fef7bbf43013
adkipnis <adkipnis@mailbox.org>
2025-11-14 14:17:13 +0100

    cosmetic changes

--- metabeta/data/single.py
+++ metabeta/data/single.py
@@ -22,28 +22,28 @@ class Prior:
     tau_ffx: torch.Tensor
     tau_eps: torch.Tensor
     tau_rfx: torch.Tensor
-    
+
     def __post_init__(self):
         self.d = len(self.tau_ffx)
         self.q = len(self.tau_rfx)
         self.sigmas_rfx = None
         self.sigma_eps = None
-    
+
     def sampleFfx(self) -> torch.Tensor:
         ffx = D.Normal(self.nu_ffx, self.tau_ffx).sample()
         return ffx
-    
+
     def sampleRfx(self, m: int) -> torch.Tensor:
         sigmas_rfx = D.HalfNormal(self.tau_rfx).sample()
         rfx = standardnormal(m, self.q)
         # rfx = torch.where(rfx.isnan(), 0, rfx)
-        rfx *= sigmas_rfx.unsqueeze(0)
+        rfx *= sigmas_rfx.unsqueeze(0) # type: ignore
         return rfx
-    
+
     def sampleEps(self, n: int) -> torch.Tensor:
         sigma_eps = D.HalfNormal(self.tau_eps).sample()
         eps = standardnormal(n)
-        eps *= sigma_eps
+        eps *= sigma_eps # type: ignore
         return eps
 
 
@@ -51,51 +51,51 @@ class Prior:
 @dataclass
 class Design:
     correlate: bool = False
-    
+
     def sample(self, n: int, d: int) -> torch.Tensor:
         x = torch.zeros(n, d)
         x[:, 0] = 1
         x[:, 1:] = torch.randn(n, d-1)
         return x
- 
-        
+
+
 # -----------------------------------------------------------------------------
 @dataclass
 class Generator:
     prior: Prior
     design: Design
-    n_i: list[int] # number of observations per group
-    
+    n_i: list[int] | torch.Tensor # number of observations per group
+
     def __post_init__(self):
         self.d = self.prior.d # number of ffx
         self.q = self.prior.q # number of rfx
         self.m = len(self.n_i) # number of groups
         self.n_i = torch.tensor(self.n_i) # number of observations per group
         self.n = int(self.n_i.sum()) # total number of observations
-            
-    
+ 
+
     def sample(self) -> dict[str, torch.Tensor]:
         # globals
         ffx = self.prior.sampleFfx() # (d,)
         X = self.design.sample(self.n, self.d) # (n, d)
         eps = self.prior.sampleEps(self.n) # (n,)
-        
-        # locals     
+
+        # locals
         rfx = self.prior.sampleRfx(self.m) # (m, q)
-        groups = torch.repeat_interleave(torch.arange(self.m), self.n_i)  # (n,)
+        groups = torch.repeat_interleave(torch.arange(self.m), self.n_i)  # type: ignore
         rfx_ext = rfx[groups]  # (n, q)
         Z = X[:, : self.q]
-        
+
         # outcomes
         y_hat = X @ ffx + (Z * rfx_ext).sum(dim=-1) # (n,)
         y = y_hat + eps
         rnv = eps.var() / y.var()
-        
+
         # Cov(mean Z, rfx), needed for standardization
         weighted_rfx = Z.mean(0, keepdim=True) * rfx
         cov = fullCovary(weighted_rfx)
         cov_sum = cov.sum() - cov[0, 0]
-        
+
         # outputs
         out = {
             # data
@@ -123,13 +123,13 @@ class Generator:
             "okay": torch.tensor(True),
         }
         return out
-        
+
 
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
     # standardnormal
     x = standardnormal(10, 2)
-    
+
     # prior params
     d = 3
     q = 2
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     tau_ffx = torch.randn(d).abs()
     tau_eps = torch.randn(1).abs()
     tau_rfx = torch.randn(q).abs()
-    
+
     # prior
     m = 4
     N = 100
@@ -145,13 +145,13 @@ if __name__ == '__main__':
     beta = prior.sampleFfx()
     alpha = prior.sampleRfx(m)
     noise = prior.sampleEps(N)
-    
+
     # design matrix
     design = Design()
-    
-    # generator 
+
+    # generator
     n_i = [N//m for _ in range(m)]
     gen = Generator(prior, design, n_i)
     ds = gen.sample()
-    
-    
+
+
