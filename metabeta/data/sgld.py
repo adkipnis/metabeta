84f914129e768ede2828356bdf3896443a55d7a2
adkipnis <adkipnis@mailbox.org>
2025-11-16 14:56:41 +0100

    step

--- metabeta/data/sgld.py
+++ metabeta/data/sgld.py
@@ -18,3 +18,33 @@ class SGLD:
     scaler = StandardScaler()
 
 
+    def _step(
+        self,
+        x_synth: torch.Tensor,
+        x_train: torch.Tensor,
+    ) -> torch.Tensor:
+        # detach gradient history
+        x_synth = x_synth.detach().requires_grad_(True)
+
+        # approximate energy using L2 distance
+        distances = torch.cdist(x_synth, x_train)
+        energy = distances.min(dim=1)[0] + distances.mean(1)
+        energy_sum = energy.sum()
+
+        # compute gradients
+        grad = torch.autograd.grad(
+            energy_sum,
+            x_synth,
+            create_graph=False,
+            retain_graph=False,
+            allow_unused=True,
+        )[0]
+        if grad is None:
+            grad = torch.zeros_like(x_synth)
+
+        # update step
+        noise = torch.randn_like(x_synth) * np.sqrt(2 * self.step_size)
+        return x_synth - self.step_size * grad + self.noise_scale * noise
+
+
+    def __call__(
