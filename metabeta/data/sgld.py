cc8a7b5005c3d29af012926d97353435871c983b
adkipnis <adkipnis@mailbox.org>
2025-11-16 14:56:58 +0100

    main call

--- metabeta/data/sgld.py
+++ metabeta/data/sgld.py
@@ -48,3 +48,51 @@ class SGLD:
 
 
     def __call__(
+        self,
+        X_train: np.ndarray | torch.Tensor,
+        n_samples: int = 0,
+    ) -> np.ndarray | torch.Tensor:
+        if isinstance(X_train, torch.Tensor):
+            X_train = X_train.numpy()
+            to_torch = True
+        else:
+            to_torch = False
+            
+        # prepare reference set
+        X_scaled = self.scaler.fit_transform(X_train)
+        x_train = torch.tensor(X_scaled, device=self.device, dtype=torch.float32)
+
+        # init synthetic set
+        x_std = np.std(X_scaled, axis=0)
+        noise = np.random.normal(0, x_std * 0.1, X_scaled.shape)
+        x_synth = X_scaled + noise
+        x_synth = torch.tensor(x_synth, device=self.device, dtype=torch.float32)
+
+        # optionally subsample
+        if n_samples:
+            indices = np.random.permutation(len(x_synth))[:n_samples]
+            x_synth = x_synth[indices]
+
+        # SGLD iterations with adaptive step size
+        adaptive_step_size = self.step_size
+        iterator = range(self.n_steps)
+        if self.use_tqdm:
+            iterator = tqdm(iterator)
+        for step in iterator:
+            x_synth = self._step(x_synth, x_train)
+            if step % 100 == 0:
+                adaptive_step_size *= 0.9
+            
+        # unstandardize
+        X_synth = x_synth.detach().cpu().numpy()
+        X_synth = self.scaler.inverse_transform(X_synth)
+        
+        # round previously discrete columns
+        discrete = checkDiscrete(X_train)
+        X_synth[:, discrete] = X_synth[:, discrete].round()
+        
+        # optionally back to torch
+        if to_torch:
+            X_synth = torch.tensor(X_synth)
+        return X_synth
+
