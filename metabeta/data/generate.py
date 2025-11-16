391794a911df2e435204df7002f8efc587edf95b
adkipnis <adkipnis@mailbox.org>
2025-11-16 14:54:04 +0100

    cosmetics

--- metabeta/data/generate.py
+++ metabeta/data/generate.py
@@ -14,41 +14,43 @@ from metabeta.data.markov import fitPyMC
 # -----------------------------------------------------------------------------
 # config
 def setup() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Generate hierarchical datasets.")
-    parser.add_argument("--bs_train", type=int, default=4096, help="batch size per training partition (default = 4,096).")
-    parser.add_argument("--bs_val", type=int, default=256, help="batch size for validation partition (default = 256).")
-    parser.add_argument("--bs_test", type=int, default=32, help="batch size per testing partition (default = 128).")
-    parser.add_argument("--bs_load", type=int, default=32, help="Batch size when loading (for grouping n, default = 32)")
-    parser.add_argument("--min_m", type=int, default=5, help="Minimum number of groups (default = 5).")
-    parser.add_argument("--max_m", type=int, default=30, help="Maximum number of groups (default = 30).")
-    parser.add_argument("--min_n", type=int, default=10, help="Minimum number of samples per group (default = 10).")
-    parser.add_argument("--max_n", type=int, default=70, help="Maximum number of samples per group (default = 70).")
-    parser.add_argument("--max_d", type=int, default=2, help="Maximum number of fixed effects (intercept + slopes) to draw per linear model (default = 12).")
-    parser.add_argument("--max_q", type=int, default=1, help="Maximum number of random effects (intercept + slopes) to draw per linear model (default = 4).")
-    parser.add_argument("--d_tag", type=str, default="hierarchical", help='Suffix for model ID (default = "")')
-    parser.add_argument("--toy", action="store_true", help="Generate toy data (default = False)")
-    parser.add_argument("--semi", action="store_true", help="Generate semi-synthetic data (default = False)")
-    # parser.add_argument("--varied", action="store_true", help="variable d and q (default = False)")
-    parser.add_argument("-b", "--begin", type=int, default=-1, help="Begin with iteration number #b (default = 0).")
-    parser.add_argument("-i", "--iterations", type=int, default=10, help="Number of dataset partitions to generate (default = 100, 0 only generates validation dataset).")
+    parser = argparse.ArgumentParser(description='Generate hierarchical datasets.')
+    parser.add_argument('--bs_train', type=int, default=4096, help='batch size per training partition (default = 4,096).')
+    parser.add_argument('--bs_val', type=int, default=256, help='batch size for validation partition (default = 256).')
+    parser.add_argument('--bs_test', type=int, default=128, help='batch size per testing partition (default = 128).')
+    parser.add_argument('--bs_load', type=int, default=32, help='Batch size when loading (for grouping n, default = 32)')
+    parser.add_argument('--min_m', type=int, default=5, help='Minimum number of groups (default = 5).')
+    parser.add_argument('--max_m', type=int, default=30, help='Maximum number of groups (default = 30).')
+    parser.add_argument('--min_n', type=int, default=10, help='Minimum number of samples per group (default = 10).')
+    parser.add_argument('--max_n', type=int, default=70, help='Maximum number of samples per group (default = 70).')
+    parser.add_argument('--max_d', type=int, default=3, help='Maximum number of fixed effects (intercept + slopes) to draw per linear model (default = 12).')
+    parser.add_argument('--max_q', type=int, default=1, help='Maximum number of random effects (intercept + slopes) to draw per linear model (default = 4).')
+    parser.add_argument('--d_tag', type=str, default='all', help='Suffix for model ID (default = '')')
+    parser.add_argument('--semi', action='store_false', help='Generate semi-synthetic data (default = False)')
+    parser.add_argument('--sgld', action='store_true', help='Use SGLD for semi-synthetic data (default = False)')
+    parser.add_argument('--toy', action='store_true', help='Generate toy data (default = False)')
+    # parser.add_argument('--varied', action='store_true', help='variable d and q (default = False)')
+    parser.add_argument('-s', '--seed', type=int, default=42, help='Seed for PyMC fit (default = 42)')
+    parser.add_argument('-b', '--begin', type=int, default=1, help='Begin with iteration number #b (default = 0).')
+    parser.add_argument('-i', '--iterations', type=int, default=10, help='Number of dataset partitions to generate (default = 100, 0 only generates validation dataset).')
     return parser.parse_args()
 
 # -----------------------------------------------------------------------------
 # helpers
 def getFileName(ds_type: str, part: int) -> Path:
-    if "train" in ds_type:
+    if 'train' in ds_type:
         size = cfg.bs_train
         b = cfg.bs_load
-    elif "val" in ds_type:
+    elif 'val' in ds_type:
         size = cfg.bs_val
         b = 1
-    elif "test" in ds_type:
+    elif 'test' in ds_type:
         size = cfg.bs_test
         b = 1
     else:
         raise ValueError
     fn = dsFilename(
-        fx_type="mfx",
+        fx_type='mfx',
         ds_type=ds_type,
         b=b,
         m=cfg.max_m,
@@ -65,8 +67,8 @@ def getFileName(ds_type: str, part: int) -> Path:
 
 
 def sampleInt(batch_size: int | tuple, min_val: int, max_val: int) -> torch.Tensor:
-    """sample batch from discrete uniform
-    (include the max_val as possible value)"""
+    '''sample batch from discrete uniform
+    (include the max_val as possible value)'''
     shape = (batch_size,) if isinstance(batch_size, int) else batch_size
     return torch.randint(min_val, max_val + 1, shape)
 
@@ -74,7 +76,7 @@ def sampleInt(batch_size: int | tuple, min_val: int, max_val: int) -> torch.Tens
 def sampleIntBatched(
     min_val: int | torch.Tensor, max_val: int | torch.Tensor
 ) -> torch.Tensor:
-    """batched version of the above with varying bounds"""
+    '''batched version of the above with varying bounds'''
     if isinstance(min_val, int):
         min_val = torch.tensor([min_val])
     if isinstance(max_val, int):
@@ -84,10 +86,10 @@ def sampleIntBatched(
     return samples
 
 
-def sampleHN(shape: tuple, scale: float, max_val: float = float("inf")) -> torch.Tensor:
-    """sample batch from halfnormal, optionally clamp"""
+def sampleHN(shape: tuple, scale: float, max_val: float = float('inf')) -> torch.Tensor:
+    '''sample batch from halfnormal, optionally clamp'''
     val = D.HalfNormal(scale).sample(shape)
-    if max_val < float("inf"):
+    if max_val < float('inf'):
         val = val.clamp(max=max_val) # type: ignore
     return val # type: ignore
 
@@ -101,7 +103,7 @@ def maxShapes(data: list[dict[str, torch.Tensor]]) -> dict[str, tuple[int, ...]]
     for entry in data:
         for key, tensor in entry.items():
             if not isinstance(tensor, torch.Tensor):
-                raise ValueError("expected all entries to be tensors")
+                raise ValueError('expected all entries to be tensors')
             shape = tuple(tensor.shape)
             if key not in max_shapes:
                 max_shapes[key] = shape
@@ -131,23 +133,23 @@ def aggregate(data: list[dict[str, torch.Tensor]]) -> dict[str, torch.Tensor]:
 
 
 # -----------------------------------------------------------------------------
-# generators
+# wrapper
 
 def generate(
     batch_size: int,
-    seed: int,
+    seed: int, # seed for dataset simulation, separate from pymc seed
     fit: bool = False,
     bs_load: int = 1,
 ) -> list[dict[str, torch.Tensor]]:
-    """Generate a [batch_size] list of mixed effects datasets
-    with varying n, m, d, q."""
-    
+    '''Generate a [batch_size] list of mixed effects datasets
+    with varying n, m, d, q.'''
+
     # init
     torch.manual_seed(seed)
     data = []
     iterator = tqdm(range(batch_size))
     if not fit:
-        iterator.set_description(f"{part:02d}/{cfg.iterations:02d}")
+        iterator.set_description(f'{part:02d}/{cfg.iterations:02d}')
 
     # presample sizes
     d = sampleInt(batch_size, cfg.max_d, cfg.max_d)
@@ -157,7 +159,7 @@ def generate(
     )
     n = sampleInt((batch_size, cfg.max_m), cfg.min_n, cfg.max_n)
     d, m, n, q = d.tolist(), m.tolist(), n.tolist(), q.tolist()
-    
+ 
     # presample priors
     nu_ffx = D.Uniform(-20, 20).sample((batch_size, cfg.max_d))
     tau_ffx0 = D.Uniform(1, 30).sample((batch_size, 1))
@@ -178,6 +180,8 @@ def generate(
         ds = None
         attempts = 0
         okay = False
+
+        # setup parameter sampler
         d_i, q_i, n_i = d[i], q[i], n[i][: m[i]]
         prior = Prior(nu_ffx[i, :d_i],
                       tau_ffx[i, :d_i],
@@ -205,39 +209,37 @@ def generate(
     return data
 
 
-
 # =============================================================================
-if __name__ == "__main__":
-    
+if __name__ == '__main__':
     # init outout directory
-    os.makedirs(Path("..", "outputs", "data"), exist_ok=True)
-    
+    os.makedirs(Path('..', 'outputs', 'data'), exist_ok=True)
+
     # setup config
     cfg = setup()
     assert cfg.bs_train % cfg.bs_load == 0, (
-        "storage batch size must be divisible by loading batch size"
+        'storage batch size must be divisible by loading batch size'
     )
     part = cfg.begin
     raw = None
 
     # generate test dataset
     if cfg.begin == -1:
-        print("Generating test set...")
+        print('Generating test set...')
         ds_test = generate(cfg.bs_test, part, fit=True)
         ds_test = aggregate(ds_test)
-        fn = getFileName("test", -1)
+        fn = getFileName('test', -1)
         np.savez_compressed(fn, **ds_test, allow_pickle=True)
-        print(f"\nSaved test set to {fn}")
+        print(f'\nSaved test set to {fn}')
         exit()
 
     # generate validation dataset
     if cfg.begin == 0:
-        print("Generating validation set...")
+        print('Generating validation set...')
         ds_val = generate(cfg.bs_val, part)
         ds_val = aggregate(ds_val)
-        fn = getFileName("val", 0)
+        fn = getFileName('val', 0)
         np.savez_compressed(fn, **ds_val, allow_pickle=True)
-        print(f"\nSaved validation set to {fn}")
+        print(f'\nSaved validation set to {fn}')
         cfg.begin += 1
 
     # potentially stop after that
@@ -245,10 +247,13 @@ if __name__ == "__main__":
         exit()
 
     # generate training datasets
-    print(f"Generating {cfg.iterations} training partitions of {cfg.bs_train} datasets each...")
+    print(f'Generating {cfg.iterations} training partitions of {cfg.bs_train} datasets each...')
     for part in range(cfg.begin, cfg.iterations + 1):
         ds_train = generate(cfg.bs_train, part, bs_load=cfg.bs_load)
         ds_train = aggregate(ds_train)
-        fn = getFileName("train", part)
+        # import matplotlib.pyplot as plt
+        # plt.hist(ds_train['rnv'])
+        fn = getFileName('train', part)
         np.savez_compressed(fn, **ds_train, allow_pickle=True)
-        print(f"Saved training set to {fn}")
+        print(f'Saved training set to {fn}')
+
