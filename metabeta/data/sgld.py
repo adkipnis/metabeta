d58a7ee49cc46bf71d2ed705007bc7df95e1dc58
adkipnis <adkipnis@mailbox.org>
2025-11-16 14:56:16 +0100

    numpy checker for discreteness

--- metabeta/data/sgld.py
+++ metabeta/data/sgld.py
@@ -3,3 +3,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import torch
 
+def checkDiscrete(x: np.ndarray, tol: float = 1e-12) -> torch.Tensor:
+    diffs = np.abs(x - x.round())
+    return (diffs < tol).all(axis=0)
+
