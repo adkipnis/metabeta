5dea7b2f140192398109c354cc5d61fc5ff268d0
adkipnis <adkipnis@mailbox.org>
2025-11-16 14:44:42 +0100

    update interface

--- metabeta/data/interface.py
+++ metabeta/data/interface.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import numpy as np
 import torch
 
-DATA_DIR = Path('real')
 
 class RealDataset:
     def __init__(
@@ -11,7 +10,7 @@ class RealDataset:
     ):
         # load
         if isinstance(source, Path):
-            assert source.exists(), f'file {source} does not exist'
+            assert source.exists(), 'source does not exist'
             np_dat = np.load(source, allow_pickle=True)
             self.original = {k: np_dat[k] for k in np_dat.files}
         elif isinstance(source, dict):
@@ -19,17 +18,30 @@ class RealDataset:
         else:
             raise TypeError(f'source should be either Path or dict but is {type(source)}')
 
-        # check if data is pre-grouped
-        is_grouped = self.original['groups'] is not None
-        assert is_grouped, 'ungrouped data not supported, yet'
-
         # store arrays as tensors
         self.data = {
             k: torch.from_numpy(v)
             for k, v in self.original.items()
             if isinstance(v, np.ndarray) and np.issubdtype(v.dtype, np.number)
         }
+        
+        # check dims
+        assert len(self.data['X']) == self.data['n'], 'dim mismatch (X, n)'
+        if 'groups' in self.data:
+            assert len(self.data['groups']) == len(self.data['X']), 'dim mismatch (group, X)'
+            assert len(self.data['groups'].unique()) == self.data['m'] , 'dim mismatch (group, m)'
+            assert self.data['n'] == self.data['n_i'].sum(), 'dim mismatch (n, n_i)'
+            
+        # tag data
+        if isinstance(source, Path):
+            self.data['source'] = source
 
     def __len__(self):
         return self.data['n']
 
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+    path = Path('real', 'preprocessed', 'math.npz')
+    ds = RealDataset(path)
+    
