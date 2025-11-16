b7f838ace22789990672655fd51b3d8660e3c05c
adkipnis <adkipnis@mailbox.org>
2025-11-16 14:56:30 +0100

    init sgld

--- metabeta/data/sgld.py
+++ metabeta/data/sgld.py
@@ -7,3 +7,14 @@ def checkDiscrete(x: np.ndarray, tol: float = 1e-12) -> torch.Tensor:
     diffs = np.abs(x - x.round())
     return (diffs < tol).all(axis=0)
 
+class SGLD:
+    ''' Stochastig gradient Langevin dynamics generator for X
+        simplified version of https://github.com/sebhaan/TabPFGen '''
+    n_steps: int = 250
+    step_size: float = 0.01
+    noise_scale: float = 0.01
+    device: str = 'cpu'
+    use_tqdm: bool = False
+    scaler = StandardScaler()
+
+
