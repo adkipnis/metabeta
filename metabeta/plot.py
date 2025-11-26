422e434bb1503b4cae2760e6cd134d735044e03b
adkipnis <adkipnis@mailbox.org>
2025-11-26 21:07:00 +0100

    generalize posterior plot

--- metabeta/plot.py
+++ metabeta/plot.py
@@ -71,21 +71,20 @@ def _histplot(x, **kwargs):
 def posterior(
     target: torch.Tensor,
     proposed: dict[str, torch.Tensor],
-    mcmc: dict[str, torch.Tensor],
+    other: dict[str, torch.Tensor],
     names: list[str],
     batch_idx: int = 0,
+    other_label: str = 'HMC',
     **kwargs,
 ):
     # apply target mask
     mask = target[batch_idx] != 0.0
     target_ = target[batch_idx][mask]
-    samples = proposed["samples"]
-    samples_ = samples[batch_idx][mask]
+    samples = proposed["samples"][batch_idx][mask]
+    weights = None
     if "weights" in proposed:
-        weights_ = proposed["weights"][batch_idx][mask]
-    else:
-        weights_ = None
-    mc_samples = (mcmc["samples"][batch_idx][mask],)
+        weights = proposed["weights"][batch_idx][mask]
+    other_samples = other["samples"][batch_idx][mask]
     names_ = names[mask.numpy()]
     d = int(mask.sum())
     w = int(torch.tensor(d).sqrt().ceil())
@@ -97,13 +96,13 @@ def posterior(
         ax = axs[i]
         ax.set_axisbelow(True)
         # ax.grid(True)
-        weights_i = weights_[i] if weights_ is not None else None
-        samples_i = samples_[i]
-        mc_samples_i = mc_samples[i]
+        weights_i = weights[i] if weights is not None else None
+        samples_i = samples[i]
+        other_samples_i = other_samples[i]
 
         # grid
-        x_left = min(mc_samples_i.min(), samples_i.min())
-        x_right = max(mc_samples_i.max(), samples_i.max())
+        x_left = min(other_samples_i.min(), samples_i.min())
+        x_right = max(other_samples_i.max(), samples_i.max())
         x_grid = np.linspace(x_left, x_right, 1000)
 
         # mb kde plot
@@ -112,8 +111,8 @@ def posterior(
         ax.plot(x_grid, kde(x_grid), color="darkgreen", label=label, lw=4)
 
         # mc kde plot
-        label = "HMC" if i == 0 else None
-        kde_m = gaussian_kde(mc_samples_i)
+        label = other_label if i == 0 else None
+        kde_m = gaussian_kde(other_samples_i)
         ax.plot(x_grid, kde_m(x_grid), color="darkgoldenrod", label=label, lw=4)
 
         ax.axvline(
