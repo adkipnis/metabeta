e7624343df7d6d028a0bb9a49ffa2e8d8aea07ba
adkipnis <adkipnis@mailbox.org>
2025-11-30 19:02:43 +0100

    externalize model config

--- metabeta/train.py
+++ metabeta/train.py
@@ -1,13 +1,13 @@
 import os
 from pathlib import Path
+import argparse
+import yaml
 import csv
 from datetime import datetime
 from tqdm import tqdm
-import argparse
 
 import torch
 import torch.nn as nn
-# from torch.nn.utils import clip_grad_norm_
 from torch.utils.tensorboard.writer import SummaryWriter
 from torch.utils.data import DataLoader
 import schedulefree
@@ -15,58 +15,32 @@ import schedulefree
 from metabeta.utils import setDevice, dsFilename, getConsoleWidth
 from metabeta.data.dataset import getDataLoader
 from metabeta.models.approximators import Approximator, ApproximatorMFX
-from metabeta import plot
 
 
 def setup() -> argparse.Namespace:
     ''' Parse command line arguments. '''
     parser = argparse.ArgumentParser()
-
+    
     # misc
-    parser.add_argument('--m_tag', type=str, default='all', help='Suffix for model ID (default = '')')
-    parser.add_argument('-s', '--seed', type=int, default=42, help='Model seed')
-    parser.add_argument('--device', type=str, default='mps', help='Device to use [cpu, cuda, mps]')
-    parser.add_argument('--cores', type=int, default=8, help='Nubmer of processor cores to use (default = 8)')
-
-    # data
-    parser.add_argument('--d_tag', type=str, default='all', help='Suffix for data ID (default = '')')
-    # parser.add_argument('--varied', action='store_true', help='Use data with variable d/q (default = False)')
-    parser.add_argument('-t', '--fx_type', type=str, default='mfx', help='Type of dataset [ffx, mfx] (default = ffx)')
-    parser.add_argument('-d', type=int, default=3, help='Number of fixed effects (with bias, default = 5)')
-    parser.add_argument('-q', type=int, default=1, help='Number of random effects (with bias, default = 1)')
-    parser.add_argument('-m', type=int, default=30, help='Maximum number of groups (default = 50).')
-    parser.add_argument('-n', type=int, default=70, help='Maximum number of samples per group (default = 100).')
-    parser.add_argument('--permute', action='store_true', help='Permute slope variables for uniform learning across heads (default = True)')
-
+    parser.add_argument('-s', '--seed', type=int, default=42, help='model seed (default = 42)')
+    parser.add_argument('--device', type=str, default='mps', help='device to use [cpu, cuda, mps]')
+    parser.add_argument('--cores', type=int, default=8, help='nubmer of processor cores to use (default = 8)')
+    parser.add_argument('--plot', action='store_false', help='plot sampling results (default = True)')
+    
+    # loading
+    parser.add_argument('--d_tag', type=str, default='all', help='suffix for data ID (default = '')')
+    parser.add_argument('--m_tag', type=str, default='all', help='suffix for model ID (default = '')')
+    parser.add_argument('--c_tag', type=str, default='config', help='name of model config file (default = "config")')
+    parser.add_argument('-l', '--load', type=int, default=0, help='load model from iteration #p')
+    
     # training
-    parser.add_argument('--patience', type=int, default=25, help='Maximum number of iterations without improvement before Early Stopping (default = 5)')
-    parser.add_argument('--bs-train', type=int, default=4096, help='macro batch size per training partition (default = 4096).')
-    parser.add_argument('--bs-val', type=int, default=256, help='macro batch size for validation partition (default = 256).')
-    parser.add_argument('--bs-mini', type=int, default=32, help='mini batch size (default = 32)')
-    parser.add_argument('--lr', type=float, default=5e-4, help='Learning rate (Adam, default = 5e-4)')
-    parser.add_argument('--standardize', action='store_false', help='Standardize inputs (default = True)')
-    parser.add_argument('-p', '--preload', type=int, default=0, help='Preload model from iteration #p')
-    parser.add_argument('-i', '--iterations', type=int, default=50, help='Maximum number of iterations to train (default = 10)')
-
-    # summary network
-    parser.add_argument('--sum_type', type=str, default='set-transformer', help='Summarizer architecture [set-transformer, dual-transformer] (default = set-transformer)')
-    parser.add_argument('--sum_blocks', type=int, default=3, help='Number of blocks in summarizer (default = 3)')
-    parser.add_argument('--sum_d', type=int, default=128, help='Model dimension (default = 128)')
-    parser.add_argument('--sum_ff', type=int, default=128, help='Feedforward dimension (default = 128)')
-    parser.add_argument('--sum_depth', type=int, default=1, help='Feedforward layers (default = 1)')
-    parser.add_argument('--sum_out', type=int, default=64, help='Summary dimension (default = 64)')
-    parser.add_argument('--sum_heads', type=int, default=8, help='Number of heads (poolformer, default = 8)')    
-    parser.add_argument('--sum_dropout', type=float, default=0.01, help='Dropout rate (default = 0.01)')
-    parser.add_argument('--sum_act', type=str, default='GELU', help='Activation funtction [anything implemented in torch.nn] (default = GELU)')
-
-    # posterior network
-    parser.add_argument('--post_type', type=str, default='affine', help='Normalizing flow architecture [affine, spline] (default = affine)')
-    parser.add_argument('--flows', type=int, default=8, help='Number of normalizing flow blocks (default = 8)')
-    parser.add_argument('--post_ff', type=int, default=256, help='Feedforward dimension (default = 256)')
-    parser.add_argument('--post_depth', type=int, default=3, help='Feedforward layers (default = 3)')
-    parser.add_argument('--post_dropout', type=float, default=0.01, help='Dropout rate (default = 0.01)')
-    parser.add_argument('--post_act', type=str, default='ReLU', help='Activation funtction [anything implemented in torch.nn] (default = ReLU)')
-
+    parser.add_argument('-i', '--iterations', type=int, default=10, help='maximum number of iterations to train (default = 10)')
+    parser.add_argument('--patience', type=int, default=20, help='early stopping criterium (default = 20)')
+    parser.add_argument('--bs_train', type=int, default=4096, help='number of regression datasets per training set (default = 4096)')
+    parser.add_argument('--bs_val', type=int, default=256, help='number of regression datasets in validation set (default = 256)')
+    parser.add_argument('--bs_mini', type=int, default=16, help='umber of regression datasets per minibatch (default = 16)')
+    parser.add_argument('--lr', type=float, default=5e-4, help='optimizer learning rate (default = 5e-4)')
+    
     return parser.parse_args()
 
 
@@ -286,10 +260,8 @@ def validate(model: ApproximatorMFX, dl: DataLoader, step: int) -> int:
 
 
 ###############################################################################
-
 if __name__ == '__main__':
-    # -------------------------------------------------------------------------
-    # --- setup config
+    # --- setup training config
     cfg = setup()
     torch.manual_seed(cfg.seed)
     torch.cuda.manual_seed_all(cfg.seed)
@@ -298,41 +270,13 @@ if __name__ == '__main__':
     console_width = getConsoleWidth()
     device = setDevice(cfg.device)
     torch.set_num_threads(cfg.cores)
-
-    # --- set up model
-    summary_dict = {
-        'type': cfg.sum_type,
-        'd_model': cfg.sum_d,
-        'n_blocks': cfg.sum_blocks,
-        'd_ff': cfg.sum_ff,
-        'depth': cfg.sum_depth,
-        'd_output': cfg.sum_out,
-        'n_heads': cfg.sum_heads,
-        'dropout': cfg.sum_dropout,
-        'activation': cfg.sum_act,
-    }
-    posterior_dict = {
-        'type': cfg.post_type,
-        'flows': cfg.flows,
-        'd_ff': cfg.post_ff,
-        'depth': cfg.post_depth,
-        'dropout': cfg.post_dropout,
-        'activation': cfg.post_act,
-        'net_type': 'mlp',
-    }
-    model_dict = {
-        'type': cfg.fx_type,
-        'seed': cfg.seed,
-        'tag': cfg.m_tag,
-        'd': cfg.d,
-        'q': cfg.q,
-    }
-    model = ApproximatorMFX.build(
-        s_dict=summary_dict,
-        p_dict=posterior_dict,
-        m_dict=model_dict,
-        use_standardization=cfg.standardize,
-    ).to(device)
+    
+    # --- setup model
+    with open(Path('models', f'{cfg.c_tag}.yaml'), 'r') as f:
+        model_cfg = yaml.safe_load(f)
+        model_cfg['general']['seed'] = cfg.seed
+        model_cfg['general']['tag'] = cfg.m_tag
+    model = ApproximatorMFX.build(model_cfg).to(device)
     print(f'{'-' * console_width}\nmodel: {model.id}\nLearning rate: {cfg.lr}\nDevice: {device}')
 
     # --- set up optimizer
