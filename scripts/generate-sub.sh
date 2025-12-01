88958af8199d94eef97e68624146c28ad2372957
adkipnis <adkipnis@mailbox.org>
2025-12-01 19:00:16 +0100

    generate sub-sampled test

---
+++ scripts/generate-sub.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=generate-sub-3-1
+#SBATCH --output=logs/generate-sub/%j.out
+#SBATCH --error=logs/generate-sub/%j.err
+
+#SBATCH --partition cpu_p
+#SBATCH --qos cpu_normal
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=6:00:00
+#SBATCH --nice=1000
+
+source $HOME/.bashrc
+conda activate mb
+cd ../metabeta/data
+python generate.py -d 3 -q 1 -b -1 --sub
+
