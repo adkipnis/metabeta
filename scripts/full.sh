611e54a115ddcd0e1a72b5c5e9fce97fa1af8cb1
adkipnis <adkipnis@mailbox.org>
2025-12-01 19:16:29 +0100

    combined

---
+++ scripts/full.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name=full
+#SBATCH --output=logs/full/%j.out
+#SBATCH --error=logs/full/%j.err
+
+#SBATCH --partition gpu_p
+#SBATCH --qos gpu_normal
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=12:00:00
+#SBATCH --nice=1000
+
+# ---------------------------
+# Define variables
+# ---------------------------
+D=5
+Q=2
+I=50
+
+# ---------------------------
+# Load environment
+# ---------------------------
+source $HOME/.bashrc
+conda activate mb
+
+# ---------------------------
+# Run scripts with variables
+# ---------------------------
+cd ../metabeta/data
+python generate.py -d $D -q $Q -b 0 -i $I
+cd ..
+python train.py -d $D -q $Q -l 0 -i $I
+python evaluate.py -d $D -q $Q -l 0 -i $I
