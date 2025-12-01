e0f7db34283a7bcd58e7a42bf378d87b0d2aa2b2
adkipnis <adkipnis@mailbox.org>
2025-12-01 21:05:21 +0100

    update scripts

--- scripts/gen-train.sh
+++ scripts/gen-train.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 #SBATCH --job-name=gen-train-3-1
-#SBATCH --output=logs/gen-train/%j.out
-#SBATCH --error=logs/gen-train/%j.err
+#SBATCH --output=logs/gen-train-3-1/%j.out
+#SBATCH --error=logs/gen-train-3-1/%j.err
 
-#SBATCH --partition gpu_p
-#SBATCH --qos gpu_normal
+#SBATCH --partition cpu_p
+#SBATCH --qos cpu_normal
 
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --time=6:00:00
-#SBATCH --nice=1000
+#SBATCH --time=12:00:00
 
 source $HOME/.bashrc
 conda activate mb
 cd ../metabeta/data
-python generate.py -d 3 -q 1 -b 0 -i 50 --semi
+python generate.py -d 3 -q 1 -b 0 -i 100 --semi
 
