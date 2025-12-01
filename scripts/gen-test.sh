a63edda570b3a55d7072b4bace2b2facb3a62e7e
adkipnis <adkipnis@mailbox.org>
2025-12-01 20:17:56 +0100

    update gen-test

--- scripts/gen-test.sh
+++ scripts/gen-test.sh
@@ -8,10 +8,19 @@
 #SBATCH --qos cpu_normal
 
 #SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=6:00:00
 
 source $HOME/.bashrc
+
+# prevent BLAS from oversubscribing
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
+# generate data
 conda activate mb
 cd ../metabeta/data
 python generate.py -d 3 -q 1 -b -1 --semi
