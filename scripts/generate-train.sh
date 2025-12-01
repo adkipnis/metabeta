cecda327fcb8113706f015804b1cf96443640fef
adkipnis <adkipnis@mailbox.org>
2025-12-01 16:51:26 +0100

    slurm scripts

---
+++ scripts/generate-train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=generate-train-3-1
+#SBATCH --output=logs/generate-train.%j.out
+#SBATCH --error=logs/generate-train.%j.err
+
+#SBATCH --partition cpu_p
+#SBATCH --qos cpu_normal
+
+#SBATCH --nodes=4
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=6:00:00
+#SBATCH --nice=1000
+
+source $HOME/.bashrc
+conda activate mb
+python ../metabeta/data/generate.py -d 3 -q 1 -i 10
+
