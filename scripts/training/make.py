88cf3e31327417a6d4eb74b04791021dfb62093f
adkipnis <adkipnis@mailbox.org>
2025-12-01 22:26:09 +0100

    switch

--- scripts/training/make.py
+++ scripts/training/make.py
@@ -1,66 +1,33 @@
-cpu_boilerplate = '''
-#SBATCH --partition cpu_p
-#SBATCH --qos cpu_normal
+gpu_boilerplate = '''
+#SBATCH --gres=gpu:1
+#SBATCH --partition gpu_p
+#SBATCH --qos gpu_priority
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=12:00:00
 '''
-
 
 def write(path: str, content: str):
     with open(path, 'w') as f:
         f.write(content)
 
-def gen_train(d: int, q: int, i: int = 100) -> str:
+def train(d: int, q: int, i: int = 100) -> str:
     out = '#!/bin/bash'
     out += f'''
-#SBATCH --job-name=gen-train-{d}-{q}
-#SBATCH --output=logs/gen-train-{d}-{q}/%j.out
-#SBATCH --error=logs/gen-train-{d}-{q}/%j.err'''
-    out += cpu_boilerplate
+#SBATCH --job-name=train-{d}-{q}
+#SBATCH --output=logs/train-{d}-{q}/%j.out
+#SBATCH --error=logs/train-{d}-{q}/%j.err'''
+    out += gpu_boilerplate
     out += f'''
 source $HOME/.bashrc
 conda activate mb
-cd $HOME/metabeta/metabeta/data
-python generate.py -d {d} -q {q} -b 0 -i {i} --semi'''
+cd %HOME/metabeta/metabeta
+python train.py -d {d} -q {q} -l 0 -i {i} --c_tag default'''
     return out
-
-
-def gen_test(d: int, q: int) -> str:
-    out = '#!/bin/bash'
-    out += f'''
-#SBATCH --job-name=gen-test-{d}-{q}
-#SBATCH --output=logs/gen-test-{d}-{q}/%j.out
-#SBATCH --error=logs/gen-test-{d}-{q}/%j.err'''
-    out += cpu_boilerplate
-    out += f'''
-source $HOME/.bashrc
-conda activate mb
-cd $HOME/metabeta/metabeta/data
-python generate.py -d {d} -q {q} -b -1 --semi --slurm'''
-    return out
-
-
-def gen_sub(d: int, q: int) -> str:
-    out = '#!/bin/bash'
-    out += f'''
-#SBATCH --job-name=gen-sub-{d}-{q}
-#SBATCH --output=logs/gen-sub-{d}-{q}/%j.out
-#SBATCH --error=logs/gen-sub-{d}-{q}/%j.err'''
-    out += cpu_boilerplate
-    out += f'''
-source $HOME/.bashrc
-conda activate mb
-cd $HOME/metabeta/metabeta/data
-python generate.py -d {d} -q {q} -b -1 --sub --slurm'''
-    return out
-
 
 if __name__ == '__main__':
     pairs = [(3,1), (5,2), (8,3), (12, 4)]
     for d,q in pairs:
-        write(f'gen-train-{d}-{q}.sh', gen_train(d,q))
-        write(f'gen-test-{d}-{q}.sh', gen_test(d,q))
-        write(f'gen-sub-{d}-{q}.sh', gen_sub(d,q))
+        write(f'train-{d}-{q}.sh', train(d,q))
 
