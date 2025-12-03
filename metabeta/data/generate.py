195ce516994a33f81a76f35c9a97cffce705b4b0
adkipnis <adkipnis@mailbox.org>
2025-12-03 12:09:44 +0100

    add cache flag

--- metabeta/data/generate.py
+++ metabeta/data/generate.py
@@ -31,6 +31,7 @@ def setup() -> argparse.Namespace:
     parser.add_argument('--semi', action='store_true', help='Generate semi-synthetic data (default = False)')
     parser.add_argument('--sub', action='store_true', help='Generate sub-sampled real data (default = False)')
     parser.add_argument('--sgld', action='store_true', help='Use SGLD for semi-synthetic data (default = False)')
+    parser.add_argument('--cache', action='store_false', help='Save each fitted dataset separately to cache (default = True)')
     parser.add_argument('--slurm', action='store_true', help='Turn off multiprocessing for slurm (default = False)')
     parser.add_argument('-s', '--seed', type=int, default=42, help='Seed for PyMC fit (default = 42)')
     parser.add_argument('-b', '--begin', type=int, default=0, help='Begin with iteration number #b.')
