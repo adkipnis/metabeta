631700b53f8c7742b8becb98fe59f815f019bea9
adkipnis <adkipnis@mailbox.org>
2025-12-15 17:35:04 +0100

    outsource standardization

--- metabeta/simulation/simulator.py
+++ metabeta/simulation/simulator.py
@@ -5,30 +5,34 @@ from metabeta.simulation.utils import standardize
 def simulate(parameters: dict[str, np.ndarray],
              observations: dict[str, np.ndarray],
              ) -> np.ndarray:
-    # unpack
+    # unpack parameters
     ffx = parameters['ffx']
     rfx = parameters['rfx']
     sigma_rfx = parameters['sigma_rfx']
     sigma_eps = parameters['sigma_eps']
-    X = observations['X']
-    groups = observations['groups']
-
-    # dims
-    n = len(X)
     q = len(sigma_rfx)
+
+    # unpack observations
+    X = observations['X'] # assumed to be standardized
+    Z = X[:, :q]
+    groups = observations['groups']
+    n = len(X)
 
     # generate noise
     eps = np.random.normal(size=(n,))
     eps = standardize(eps, axis=0) * sigma_eps
 
-    # standardize observations
-    X_ = standardize(X, axis=0, exclude_binary=True)
-    Z_ = X_[:, :q]
-
     # outcome
-    y = X_ @ ffx + (Z_ * rfx[groups]).sum(-1) + eps
+    y = X @ ffx + (Z * rfx[groups]).sum(-1) + eps
     return y
 
+
+    # # standardize observations
+    # X_ = standardize(X, axis=0, exclude_binary=True)
+    # Z_ = X_[:, :q]
+    #
+    # # outcome
+    # y = X_ @ ffx + (Z_ * rfx[groups]).sum(-1) + eps
 
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
