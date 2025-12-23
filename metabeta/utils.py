13d24dcaf722a1a5b8e7cc8672ef04e230342b49
adkipnis <adkipnis@mailbox.org>
2025-12-23 13:33:56 +0100

    log uniform util

--- metabeta/utils.py
+++ metabeta/utils.py
@@ -38,3 +38,16 @@ def dataname(
     return f'{fx_type}-{p_type}-{tag}d={d}-q={q}-m={m}-n={n}-b={b}{part}.npz'
 
 # --- sampling
+def logUniform(a: float,
+               b: float,
+               add: float = 0.0,
+               round: bool = False) -> float|int:
+    assert a > 0, 'lower bound must be positive'
+    assert b > a, 'upper bound must be larger than lower bound'
+    out = np.exp(np.random.uniform(np.log(a), np.log(b)))
+    out += add
+    if round:
+        return int(np.round(out))
+    return float(out)
+
+
