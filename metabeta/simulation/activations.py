733aff87507b26c3f63de44f0e308c4a445b2cd1
adkipnis <adkipnis@mailbox.org>
2025-12-23 13:34:30 +0100

    processing layers

--- metabeta/simulation/activations.py
+++ metabeta/simulation/activations.py
@@ -6,3 +6,34 @@ from torch import distributions as D
 from torch.nn import functional as F
 from metabeta.utils import logUniform
 
+# --- preprocessing layers
+class Standardizer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mean = None
+        self.std = None
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.mean is None or self.std is None:
+            self.mean = x.mean(dim=0, keepdim=True)
+            self.std = x.std(dim=0, keepdim=True) + 1e-6
+        return (x - self.mean) / self.std
+
+class RandomScale(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.scale = (2 * torch.randn(1)).exp()
+        self.bias = torch.randn(1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.scale * (x + self.bias)
+
+class RandomScaleFactory:
+    def __init__(self, act: nn.Module):
+        self.act = act
+
+    def __call__(self) -> nn.Module:
+        return nn.Sequential(Standardizer(), RandomScale(), self.act())
+
+
+# --- random choice layers
