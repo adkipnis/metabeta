d6b265313637a390b010b82cce359a4b9ac520da
adkipnis <adkipnis@mailbox.org>
2025-12-23 13:33:40 +0100

    seed util

--- metabeta/utils.py
+++ metabeta/utils.py
@@ -1,8 +1,18 @@
+from pathlib import Path
+import numpy as np
 import torch
 
+# --- setters and getters
 def getDevice() -> str:
     if torch.cuda.is_available():
         return 'gpu'
     elif torch.backends.mps.is_available():
         return 'mps'
     return 'cpu'
+
+def setSeed(s: int) -> np.random.Generator:
+    torch.manual_seed(s)
+    np.random.seed(s)
+    rng = np.random.default_rng(s)
+    return rng
+
