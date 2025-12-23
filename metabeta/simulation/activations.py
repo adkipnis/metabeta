db94f23b0dc5d9294a7327f8de6724bd00175ad8
adkipnis <adkipnis@mailbox.org>
2025-12-23 13:35:11 +0100

    stochastic layers

--- metabeta/simulation/activations.py
+++ metabeta/simulation/activations.py
@@ -127,3 +127,28 @@ class UnitInterval(nn.Module):
         return torch.where(x.abs() <= 1, 1., 0.)
 
 # --- probabilistic activations
+class Bernoulli(nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.bernoulli(torch.sigmoid(x))
+
+class Poisson(nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.poisson(torch.where(x > 0, x**2, 0))
+
+class Geometric(nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        p = torch.sigmoid(x) + 1e-9
+        return D.Geometric(probs=p).sample().sqrt()
+
+class Gamma(nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return D.Gamma(2, x.exp()).sample().sqrt()
+
+class Beta(nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        m = torch.sigmoid(x + 1e-9)
+        alpha = m * 10
+        beta = (1-m) * 10
+        return D.Beta(alpha, beta).sample()
+
+# --- GP based activations
