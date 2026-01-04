996a2f7af05eee0cd0f488c7abc3f8e5c3d33f32
adkipnis <adkipnis@mailbox.org>
2026-01-04 20:38:14 +0100

    activations

--- metabeta/models/mlp.py
+++ metabeta/models/mlp.py
@@ -47,3 +47,25 @@ def zeroInitializer(layer: nn.Module, limit: float = 1e-3) -> None:
 
 
 # --- activations
+class GeGLU(nn.Module):
+    ''' GELU-based Gated Linear Unit '''
+    def forward(self, x): # 2d -> d
+        x, gate = x.chunk(2, dim=-1)
+        return x * nn.functional.gelu(gate)
+
+ACTIVATIONS = {
+    'ReLU': nn.ReLU,
+    'LeakyReLU': nn.LeakyReLU,
+    'SiLU': nn.SiLU,
+    'Mish': nn.Mish,
+    'ELU': nn.ELU, # default for NF
+    'GELU': nn.GELU, # default for Transformers
+    'GeGLU': GeGLU, # optional for Set Transformers
+}
+
+def getActivation(name: str) -> nn.Module:
+    assert name in ACTIVATIONS, f'Unknown activation {name}'
+    return ACTIVATIONS[name]()
+
+
+# --- multi-layer perceptron
