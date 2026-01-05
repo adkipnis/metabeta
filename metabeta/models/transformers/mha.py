d83e354ce3a1b1042c096abcc64c9031e97fe7ec
adkipnis <adkipnis@mailbox.org>
2026-01-05 12:42:43 +0100

    forward pass

--- metabeta/models/transformers/mha.py
+++ metabeta/models/transformers/mha.py
@@ -14,3 +14,40 @@ class MHA(nn.Module):
         self.mha = nn.MultiheadAttention(
             d_model, n_heads, dropout, batch_first=True)
 
+    def forward(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor | None = None,
+        value: torch.Tensor | None = None,
+        mask: torch.Tensor | None = None,
+    ) -> torch.Tensor:
+        # prepare mask
+        kpd = None
+        if mask is not None:
+            kpd = ~mask
+
+        # prepare inputs
+        if key is None:
+            key = query
+        if value is None:
+            value = key
+
+        # deal with left-over dimensions
+        shape = None
+        if len(query.shape) > 3:
+            shape = query.shape
+            query = query.reshape(-1, *query.shape[-2:])
+            key = key.reshape(-1, *key.shape[-2:])
+            value = value.reshape(-1, *value.shape[-2:])
+            if kpd is not None:
+                kpd = kpd.reshape(-1, *kpd.shape[-1:])
+
+        # forward pass
+        h, _ = self.mha(query, key, value,
+                        key_padding_mask=kpd)
+ 
+        # optionally reshape outputs
+        if shape is not None:
+            h = h.view(shape)
+        return h
+
