58a4a3b23f793b24e42e01472c235c55c0837219
adkipnis <adkipnis@mailbox.org>
2026-01-05 19:47:52 +0100

    update relative import

--- metabeta/models/feedforward/residual.py
+++ metabeta/models/feedforward/residual.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from metabeta.models.feedforward.utils import (
+from metabeta.models.utils import (
     getActivation, getInitializer,
     zeroInitializer, lastZeroInitializer, weightNormInitializer)
 
