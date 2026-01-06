96c6232b64d2297d3ba9d4d30629acd510a5cb54
adkipnis <adkipnis@mailbox.org>
2026-01-06 23:14:15 +0100

    init dual coupling

--- metabeta/models/normalizingflows/coupling.py
+++ metabeta/models/normalizingflows/coupling.py
@@ -34,3 +34,35 @@ class Coupling(nn.Module):
     def inverse(self, x1, x2, condition=None, mask2=None):
         return self(x1, x2, condition, mask2, inverse=True)
 
+
+class DualCoupling(nn.Module):
+    ''' Dual Coupling Step:
+        1. Split inputs along pivot
+        2. Apply first coupling step to split
+        3. Apply second coupling step to swapped outputs
+        4. return joint outputs and log determinants
+    '''
+    def __init__(
+        self,
+        d_data: int,
+        d_context: int = 0,
+        net_kwargs: dict = {},
+        transform: str = 'affine',
+    ):
+        super().__init__()
+        self.pivot = d_data // 2
+        split_dims = (self.pivot, d_data - self.pivot)
+        self.coupling1 = Coupling(
+            split_dims=split_dims,
+            d_context=d_context,
+            net_kwargs=net_kwargs,
+            transform=transform,
+        )
+        self.coupling2 = Coupling(
+            split_dims=(split_dims[1], split_dims[0]),
+            d_context=d_context,
+            net_kwargs=net_kwargs,
+            transform=transform,
+        )
+
+    def __call__(self, x: torch.Tensor,
