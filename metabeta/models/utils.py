e5986a60839ee191bd692b8febd07db303d12c3b
adkipnis <adkipnis@mailbox.org>
2026-01-06 22:53:45 +0100

    mark globals

--- metabeta/models/utils.py
+++ metabeta/models/utils.py
@@ -17,18 +17,18 @@ def lecun_uniform(weight: torch.Tensor) -> torch.Tensor:
     limit = (3.0 / fan_in) ** 0.5
     return nn.init.uniform_(weight, -limit, limit)
 
-idict = {m: dict() for m in METHODS}
-idict['kaiming']['uniform'] = nn.init.kaiming_uniform_
-idict['kaiming']['normal'] = nn.init.kaiming_normal_
-idict['xavier']['uniform'] = nn.init.xavier_uniform_
-idict['xavier']['normal'] = nn.init.xavier_normal_
-idict['lecun']['uniform'] = lecun_uniform
-idict['lecun']['normal'] = lecun_normal
+IDICT = {m: dict() for m in METHODS}
+IDICT['kaiming']['uniform'] = nn.init.kaiming_uniform_
+IDICT['kaiming']['normal'] = nn.init.kaiming_normal_
+IDICT['xavier']['uniform'] = nn.init.xavier_uniform_
+IDICT['xavier']['normal'] = nn.init.xavier_normal_
+IDICT['lecun']['uniform'] = lecun_uniform
+IDICT['lecun']['normal'] = lecun_normal
 
 def getInitializer(method: str, distribution: str) -> Initializer:
     assert method in METHODS, 'unknown method'
     assert distribution in DISTRIBUTIONS, 'unknown distribution'
-    init_func = idict[method][distribution]
+    init_func = IDICT[method][distribution]
 
     def initializer(layer: nn.Module) -> None:
         if isinstance(layer, nn.Linear):
