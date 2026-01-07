74ea7759e3de27ac59dce07c6549f0cabad8d284
adkipnis <adkipnis@mailbox.org>
2026-01-07 18:25:02 +0100

    actnorm

--- metabeta/models/normalizingflows/basictransforms.py
+++ metabeta/models/normalizingflows/basictransforms.py
@@ -5,3 +5,25 @@ from torch.nn import functional as F
 from metabeta.models.normalizingflows import Transform
 
 
+class ActNorm(Transform):
+    def __init__(self, d_target: int):
+        super().__init__()
+        self.scale = nn.Parameter(torch.ones((d_target,)))
+        self.bias = nn.Parameter(torch.zeros((d_target,)))
+
+    def __call__(self, x, condition=None, mask=None, inverse=False):
+        scale, bias = self.scale.expand_as(x), self.bias.expand_as(x)
+        if mask is not None:
+            bias = bias * mask
+        if inverse:
+            x = (x - bias) / scale
+        else:
+            x = scale * x + bias
+        log_det = scale.abs().log()
+        if inverse:
+            log_det = -log_det
+        if mask is not None:
+            log_det = log_det * mask
+        return x, log_det.sum(-1), mask
+
+
