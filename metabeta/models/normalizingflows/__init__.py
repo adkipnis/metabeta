de0c2fbc55130153749f378d4a2c7c4267cc2e39
adkipnis <adkipnis@mailbox.org>
2026-01-07 18:26:59 +0100

    update __init__

--- metabeta/models/normalizingflows/__init__.py
+++ metabeta/models/normalizingflows/__init__.py
@@ -1,3 +1,5 @@
+from .distributions import  BaseDist
+from .basictransforms import Transform, ActNorm, Permute, LU
 from .couplingtransforms import CouplingTransform, Affine
 
-__all__ = ['CouplingTransform', 'Affine']
+__all__ = ['Transform', 'ActNorm', 'Permute', 'LU', 'BaseDist', 'CouplingTransform', 'Affine']
