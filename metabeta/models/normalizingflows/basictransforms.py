1530cea8db79b32a6254cd3d1f622bab5ab135f5
adkipnis <adkipnis@mailbox.org>
2026-01-07 18:26:47 +0100

    move base transform class

--- metabeta/models/normalizingflows/basictransforms.py
+++ metabeta/models/normalizingflows/basictransforms.py
@@ -1,8 +1,29 @@
+from abc import abstractmethod
 import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
-from metabeta.models.normalizingflows import Transform
+
+
+class Transform(nn.Module):
+    ''' Base template for transforms used in normalizing flows '''
+    @abstractmethod
+    def __call__(self, x: torch.Tensor,
+                 condition: torch.Tensor | None = None,
+                 mask: torch.Tensor | None = None,
+                 inverse: bool = False,
+                 ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]:
+        ...
+
+    def forward(self, x, condition=None, mask=None):
+        return self(x, condition, mask, inverse=False)
+
+    def inverse(self, x, condition=None, mask=None):
+        return self(x, condition, mask, inverse=True)
+
+    def _forwardMask(self, mask: torch.Tensor):
+        return mask
+
 
 
 class ActNorm(Transform):
