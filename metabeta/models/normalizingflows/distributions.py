b5f61fcb595223adcca70b3bff79e86ef8e23b1c
adkipnis <adkipnis@mailbox.org>
2026-01-07 13:44:55 +0100

    properties

--- metabeta/models/normalizingflows/distributions.py
+++ metabeta/models/normalizingflows/distributions.py
@@ -16,3 +16,28 @@ class StaticDist(nn.Module):
         self._training_dist = self._dist['torch'](**self._default_params)
         self.device = self._training_dist.mean.device
 
+    def __repr__(self) -> str:
+        params = ''
+        for k,v in self._default_params.items():
+            params += f'{k}: {v:.1f}, '
+        return f'Static {self.family.title()} ({params[:-2]})'
+
+    @property
+    def _default_params(self) -> dict[str, float]:
+        out = {}
+        if self.family == 'student':
+            out['df'] = 5.
+        out['loc'] = 0.
+        out['scale'] = 1.
+        return out
+
+    @property
+    def _dist(self) -> dict:
+        if self.family == 'normal':
+            return {'torch': D.Normal, 'scipy': scipy.stats.norm} 
+        elif self.family == 'student':
+            return {'torch': D.StudentT, 'scipy': scipy.stats.t}
+        else:
+            raise ValueError
+
+    def sample(self, shape: tuple[int, ...]) -> torch.Tensor:
