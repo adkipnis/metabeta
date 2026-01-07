1555c2a2372c98c90fb710672d27e9c9006507bd
adkipnis <adkipnis@mailbox.org>
2026-01-07 18:25:21 +0100

    LU

--- metabeta/models/normalizingflows/basictransforms.py
+++ metabeta/models/normalizingflows/basictransforms.py
@@ -49,3 +49,96 @@ class Permute(Transform):
         return mask
 
 
+class LU(Transform):
+    ''' rewrite of LU from https://github.com/bayesiains/nflows
+        implicit masking is done by setting the corresponding rows and columns of L and U to unit vectors
+    '''
+    def __init__(self, d_target: int, identity_init: bool = True, eps: float = 1e-3):
+        super().__init__()
+        self.d_target = d_target
+        self.eps = eps
+
+        # indices for triangular matrices
+        self.lower_indices = np.tril_indices(d_target, k=-1)
+        self.upper_indices = np.triu_indices(d_target, k=1)
+        self.diag_indices = np.diag_indices(d_target)
+
+        # learnable (but input-independent) parameters
+        n_triangular_entries = ((d_target - 1) * d_target) // 2
+        self.lower_entries = nn.Parameter(torch.zeros(n_triangular_entries))
+        self.upper_entries = nn.Parameter(torch.zeros(n_triangular_entries))
+        self.unconstrained_upper_diag = nn.Parameter(torch.zeros(d_target))
+        self.bias = nn.Parameter(torch.zeros(d_target))
+
+        # init parameters
+        nn.init.zeros_(self.bias)
+        if identity_init:
+            nn.init.zeros_(self.lower_entries)
+            nn.init.zeros_(self.upper_entries)
+            constant = np.log(np.exp(1 - self.eps) - 1)
+            nn.init.constant_(self.unconstrained_upper_diag, constant)
+        else:
+            stdv = 1.0 / np.sqrt(d_target)
+            nn.init.uniform_(self.lower_entries, -stdv, stdv)
+            nn.init.uniform_(self.upper_entries, -stdv, stdv)
+            nn.init.uniform_(self.unconstrained_upper_diag, -stdv, stdv)
+
+    @property
+    def upper_diag(self):
+        return F.softplus(self.unconstrained_upper_diag) + self.eps
+
+    def _getLU(self, mask: torch.Tensor | None, b: int, d: int):
+        device = self.bias.device
+
+        # construct
+        lower = torch.zeros(self.d_target, self.d_target, device=device)
+        lower[self.lower_indices[0], self.lower_indices[1]] = self.lower_entries
+        lower[self.diag_indices[0], self.diag_indices[1]] = (
+            1.0  # WLOG because the diagonal of U is can absorb the diagonal of L
+        )
+        upper = torch.zeros(self.d_target, self.d_target, device=device)
+        upper[self.upper_indices[0], self.upper_indices[1]] = self.upper_entries
+        upper[self.diag_indices[0], self.diag_indices[1]] = self.upper_diag
+
+        # expand and mask
+        lower = lower.unsqueeze(0).expand(b, d, d)
+        upper = upper.unsqueeze(0).expand(b, d, d)
+        bias = self.bias.unsqueeze(0).expand(b, d)
+        if mask is not None:
+            mask_ = mask.bool().unsqueeze(1) & mask.bool().unsqueeze(2)
+            eye = torch.eye(d).expand(b, -1, -1).to(device)
+            upper = torch.where(mask_, upper, eye)
+            lower = torch.where(mask_, lower, eye)
+            bias = bias * mask
+        return lower, upper, bias
+
+    def __call__(self, x, condition=None, mask=None, inverse=False):
+        # handle 3D inputs
+        b, *_, d = x.shape
+        if x.dim() == 3:
+            x = x.reshape(-1, d)
+            if mask is not None:
+                mask = mask.reshape(-1, d)
+        elif x.dim() > 3:
+            raise NotImplementedError
+
+        # multiply x with invertible matrix
+        lower, upper, bias = self._getLU(mask, b, d)
+        log_det = upper.diagonal(dim1=-2, dim2=-1).log().sum(-1)
+        if inverse:
+            x = (x - bias).unsqueeze(-1)
+            x = torch.linalg.solve_triangular(lower, x, upper=False)
+            x = torch.linalg.solve_triangular(upper, x, upper=True).squeeze(-1)
+            log_det = -log_det
+        else:
+            x = torch.einsum("bij,bj->bi", upper, x)
+            x = torch.einsum("bij,bj->bi", lower, x) + bias
+
+        # reshape back if 
+        if len(x) != b:
+            x = x.reshape(b, -1, d)
+            log_det = log_det.reshape(b, -1)
+            if mask is not None:
+                mask = mask.reshape(b, -1, d)
+        return x, log_det, mask
+
