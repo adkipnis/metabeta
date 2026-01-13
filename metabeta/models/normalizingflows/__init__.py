be454254ba48bb644c835c7aaca5f83f0f16aa50
adkipnis <adkipnis@mailbox.org>
2026-01-13 17:59:58 +0100

    include couplingflow in __init__

--- metabeta/models/normalizingflows/__init__.py
+++ metabeta/models/normalizingflows/__init__.py
@@ -1,9 +1,11 @@
 from .distributions import  BaseDist
 from .basictransforms import Transform, ActNorm, Permute, LU
 from .couplingtransforms import CouplingTransform, Affine, RationalQuadratic
+from .coupling import CouplingFlow
 
 __all__ = [
     'BaseDist',
     'Transform', 'ActNorm', 'Permute', 'LU',
     'CouplingTransform', 'Affine', 'RationalQuadratic',
+    'CouplingFlow'
 ]
