9e88a9c60ca6fa673feecac7038c0a3149416804
adkipnis <adkipnis@mailbox.org>
2026-01-13 17:56:36 +0100

    logger util

---
+++ metabeta/utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+def setupLogging(verbosity: int) -> None:
+    level_map = {
+        0: logging.WARNING,
+        1: logging.INFO,
+        2: logging.DEBUG,
+    }
+
+    log_level = level_map.get(verbosity, logging.DEBUG)
+
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        datefmt="%H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
