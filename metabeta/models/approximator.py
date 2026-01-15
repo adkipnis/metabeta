f38c72beeda5388b9eb5e216a16355e2513c77b5
adkipnis <adkipnis@mailbox.org>
2026-01-15 19:26:31 +0100

    update build

--- metabeta/models/approximator.py
+++ metabeta/models/approximator.py
@@ -42,38 +42,40 @@ class Config:
 
 
 class Approximator(nn.Module):
-    def __init__(self, cfg: dict):
+    def __init__(self, cfg: Config):
         super().__init__()
         self.cfg = cfg
+        self.d_ffx = cfg.d_ffx # number of fixed effects
+        self.d_rfx = cfg.d_rfx # number of random effects
         self.build()
 
     def build(self) -> None:
-        d_ffx = self.cfg['d']
-        d_rfx = self.cfg['q']
+        d_ffx = self.d_ffx
+        d_rfx = self.d_rfx
 
         # --- summarizers
-        s_cfg = self.cfg['summarizer']
-        if s_cfg['type'] == 'set-transformer':
+        s_cfg = self.cfg.summarizer
+        if s_cfg.type == 'set-transformer':
             Summarizer = SetTransformer
         else:
             raise NotImplementedError('unknown summarizer type')
         d_input_l = 1 + (d_ffx - 1) + (d_rfx - 1)
-        d_input_g = s_cfg['d_output'] + 1 # n_obs per group
-        self.summarizer_l = Summarizer(d_input=d_input_l, **s_cfg)
-        self.summarizer_g = Summarizer(d_input=d_input_g, **s_cfg)
+        d_input_g = s_cfg.d_output + 1 # n_obs per group
+        self.summarizer_l = Summarizer(d_input=d_input_l, **s_cfg.to_dict())
+        self.summarizer_g = Summarizer(d_input=d_input_g, **s_cfg.to_dict())
 
         # --- posteriors
-        p_cfg = self.cfg['posterior']
-        if p_cfg['type'] == 'flow':
+        p_cfg = self.cfg.posterior
+        if p_cfg.type == 'flow':
             Posterior = CouplingFlow
         else:
             raise NotImplementedError('unknown posterior type')
         d_var = 1 + d_rfx # number of variance params
         d_prior = 2 * d_ffx + d_var # number of prior params
-        d_context_g = s_cfg['d_output'] + d_prior + 2 # global summary, prior, n_groups, n_total
+        d_context_g = s_cfg.d_output + d_prior + 2 # global summary, prior, n_groups, n_total
         d_context_l = d_ffx + d_var + d_input_g # global params, local summaries
-        self.posterior_g = Posterior(d_ffx+d_var, d_context_g, **p_cfg)
-        self.posterior_l = Posterior(d_rfx, d_context_l, **p_cfg)
+        self.posterior_g = Posterior(d_ffx+d_var, d_context_g, **p_cfg.to_dict())
+        self.posterior_l = Posterior(d_rfx, d_context_l, **p_cfg.to_dict())
 
     @property
     def device(self):
