e352687f5e7a09468459e78a49424cc4aecaeec6
adkipnis <adkipnis@mailbox.org>
2026-01-20 21:33:42 +0100

    subroutine for aggregation

--- metabeta/simulation/generate.py
+++ metabeta/simulation/generate.py
@@ -45,3 +45,24 @@ class Generator:
     def __post_init__(self):
         self.outdir.mkdir(parents=True, exist_ok=True)
 
+    @staticmethod
+    def _maxShapes(batch: list[dict[str, np.ndarray]]) -> dict[str, tuple[int, ...]]:
+        ''' for each array in dataset, get the maximal shape over the whole batch '''
+        out = {}
+        for dataset in batch:
+            for key, array in dataset.items():
+                if not isinstance(array, np.ndarray):
+                    raise ValueError('expected all entries to be arrays')
+                shape = tuple(array.shape)
+                if key not in out:
+                    out[key] = shape
+                    continue
+                assert len(shape) == len(out[key]), 'ndim mismatch'
+                # Expand max_shapes[key] tuple elementwise to max dimension
+                out[key] = tuple(
+                    max(old_dim, new_dim)
+                    for old_dim, new_dim in zip(out[key], shape)
+                )
+        return out
+
+
