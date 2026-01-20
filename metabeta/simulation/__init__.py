f06df83d42d8943046cf8bf9f072d85cfd9d5ed7
adkipnis <adkipnis@mailbox.org>
2026-01-20 16:34:00 +0100

    include simulator in init

--- metabeta/simulation/__init__.py
+++ metabeta/simulation/__init__.py
@@ -1,9 +1,11 @@
 from .prior import Prior, hypersample
 from .synthesizer import Synthesizer
 from .emulator import Emulator
+from .simulator import Simulator, simulate
 
 __all__ = [
     'Prior', 'hypersample',
     'Synthesizer',
     'Emulator',
+    'Simulator', 'simulate',
 ]
