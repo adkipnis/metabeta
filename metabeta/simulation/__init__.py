86f7af273fadbb7a5ca099c30615d525eb37b2ea
adkipnis <adkipnis@mailbox.org>
2026-01-20 21:50:11 +0100

    include generator in __init__

--- metabeta/simulation/__init__.py
+++ metabeta/simulation/__init__.py
@@ -2,10 +2,11 @@ from .prior import Prior, hypersample
 from .synthesizer import Synthesizer
 from .emulator import Emulator
 from .simulator import Simulator, simulate
+from .generate import Generator
 
 __all__ = [
     'Prior', 'hypersample',
-    'Synthesizer',
-    'Emulator',
-    'Simulator', 'simulate',
+    'Synthesizer', 'Emulator', # predictors
+    'Simulator', 'simulate', # outcomes
+    'Generator', # full datasets
 ]
