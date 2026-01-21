50d2a83fb4ecb3f800d5794e3deac6415728b2ec
adkipnis <adkipnis@mailbox.org>
2026-01-21 17:51:57 +0100

    update parser

--- metabeta/simulation/fit.py
+++ metabeta/simulation/fit.py
@@ -8,20 +8,24 @@ import bambi as bmb
 import arviz as az
 
 from metabeta.utils.io import datasetFilename
+from metabeta.utils.padding import aggregate
 
 
 def setup() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Fit hierarchical datasets with Bambi.')
-    # data dimensions
+    # data
     parser.add_argument('-d', '--max_d', type=int, default=3, help='Maximum number of fixed effects (intercept + slopes) to draw per linear model (default = 16).')
     parser.add_argument('-q', '--max_q', type=int, default=1, help='Maximum number of random effects (intercept + slopes) to draw per linear model (default = 4).')
     parser.add_argument('--min_m', type=int, default=5, help='Minimum number of groups (default = 5).')
     parser.add_argument('--max_m', type=int, default=30, help='Maximum number of groups (default = 30).')
     parser.add_argument('--min_n', type=int, default=10, help='Minimum number of samples per group (default = 10).')
     parser.add_argument('--max_n', type=int, default=70, help='Maximum number of samples per group (default = 70).')
-    # partitions and sources
     parser.add_argument('--type', type=str, default='toy', help='Type of predictors [toy, flat, scm, sampled], (default = toy)')
     parser.add_argument('--source', type=str, default='all', help='Source dataset if type==sampled (default = all)')
+    parser.add_argument('--idx', type=int, default=0, help='Index of dataset in batch, for which we want to fit the data (default = 0)')
+    parser.add_argument('--reintegrate', action='store_true', help='Check if fits exist for each dataset and reintegrate into batch (default = False)')
+    parser.add_argument('--cleanup', action='store_true', help='Delete the cached fit files after succesful reintegration (default = False)')
+
     # bambi
     parser.add_argument('--respecify_ffx', action='store_true', help='Use automatic fixed effects priors by bambi instead of known (default = False)')
     parser.add_argument('--method', type=str, default='advi', help='Inference method for bambi [nuts, advi], (default = nuts)')
