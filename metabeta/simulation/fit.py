c8f7fceec62b414bc1932c305a6924b8c65b2f2d
adkipnis <adkipnis@mailbox.org>
2026-01-21 14:23:06 +0100

    parser

--- metabeta/simulation/fit.py
+++ metabeta/simulation/fit.py
@@ -8,3 +8,19 @@ import bambi as bmb
 from metabeta.utils.io import datasetFilename
 
 
+def setup() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Fit hierarchical datasets with Bambi.')
+    # data dimensions
+    parser.add_argument('-d', '--max_d', type=int, default=3, help='Maximum number of fixed effects (intercept + slopes) to draw per linear model (default = 16).')
+    parser.add_argument('-q', '--max_q', type=int, default=1, help='Maximum number of random effects (intercept + slopes) to draw per linear model (default = 4).')
+    parser.add_argument('--min_m', type=int, default=5, help='Minimum number of groups (default = 5).')
+    parser.add_argument('--max_m', type=int, default=30, help='Maximum number of groups (default = 30).')
+    parser.add_argument('--min_n', type=int, default=10, help='Minimum number of samples per group (default = 10).')
+    parser.add_argument('--max_n', type=int, default=70, help='Maximum number of samples per group (default = 70).')
+    # partitions and sources
+    parser.add_argument('--type', type=str, default='toy', help='Type of predictors [toy, flat, scm, sampled], (default = toy)')
+    parser.add_argument('--source', type=str, default='all', help='Source dataset if type==sampled (default = all)')
+    # parser.add_argument('--loop', action='store_true', help='Loop dataset sampling instead of parallelizing it with joblib (default = False)')
+    return parser.parse_args()
+
+
