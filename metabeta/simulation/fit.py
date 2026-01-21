28b9eebce5b73ba5964a622c573bd30c021a3276
adkipnis <adkipnis@mailbox.org>
2026-01-21 14:23:01 +0100

    init

---
+++ metabeta/simulation/fit.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+import argparse
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import bambi as bmb
+
+from metabeta.utils.io import datasetFilename
+
+
