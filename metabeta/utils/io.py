f594709f05f3b45c581793754b3d203bdd6974f0
adkipnis <adkipnis@mailbox.org>
2026-02-06 22:58:38 +0100

    update dsfilename

--- metabeta/utils/io.py
+++ metabeta/utils/io.py
@@ -15,11 +15,11 @@ def setSeed(s: int) -> np.random.Generator:
     rng = np.random.default_rng(s)
     return rng
 
-def datasetFilename(args: Namespace, epoch: int = 0) -> str:
+def datasetFilename(args: Namespace, partition: str,  epoch: int = 0) -> str:
  
     # partition type and optionally epoch
-    parts = [args.partition]
-    if args.partition == 'train':
+    parts = [partition]
+    if partition == 'train':
         parts.append(f'ep{epoch:04d}')
 
     # sizes
@@ -28,11 +28,11 @@ def datasetFilename(args: Namespace, epoch: int = 0) -> str:
         f'q{args.max_q}',
         f'm{args.min_m}-{args.max_m}',
         f'n{args.min_n}-{args.max_n}',
-        args.type,
+        args.ds_type,
     ]
 
     # source of sampled data
-    if args.type == 'sampled':
+    if args.ds_type == 'sampled':
         parts.append(args.source)
 
     return '_'.join(parts) + '.npz'
