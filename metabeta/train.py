7b3741a1d8f13e80dd97f333a7ccf7445bb9399d
adkipnis <adkipnis@mailbox.org>
2026-02-06 23:02:46 +0100

    argparse

--- metabeta/train.py
+++ metabeta/train.py
@@ -14,3 +14,37 @@ from metabeta.utils.dataloader import Dataloader, toDevice
 from metabeta.utils.io import setDevice, datasetFilename
 
 
+def setup() -> argparse.Namespace:
+    ''' Parse command line arguments. '''
+    parser = argparse.ArgumentParser()
+ 
+    # misc
+    parser.add_argument('-s', '--seed', type=int, default=42, help='model seed (default = 42)')
+    parser.add_argument('--reproducible', action='store_false', help='use deterministic learning trajectory (default = True)')
+    parser.add_argument('--cores', type=int, default=8, help='number of processor cores to use (default = 8)')
+    parser.add_argument('--device', type=str, default='cpu', help='device to use [cpu, cuda, mps], (default = mps)')
+    # parser.add_argument('--save', type=int, default=10, help='save model every #p iterations (default = 10)')
+
+    # model
+    parser.add_argument('--ds_type', type=str, default='toy', help='type of predictors [toy, flat, scm, sampled], (default = toy)')
+    parser.add_argument('--cfg', type=str, default='default', help='name of model config yml file')
+    parser.add_argument('-l', '--load', type=int, default=0, help='load model from iteration #l')
+ 
+    # data dimensions
+    parser.add_argument('-d', '--max_d', type=int, default=3, help='maximum number of fixed effects (intercept + slopes) to draw per linear model (default = 16).')
+    parser.add_argument('-q', '--max_q', type=int, default=1, help='maximum number of random effects (intercept + slopes) to draw per linear model (default = 4).')
+    parser.add_argument('--min_m', type=int, default=5, help='minimum number of groups (default = 5).')
+    parser.add_argument('--max_m', type=int, default=30, help='maximum number of groups (default = 30).')
+    parser.add_argument('--min_n', type=int, default=10, help='minimum number of samples per group (default = 10).')
+    parser.add_argument('--max_n', type=int, default=70, help='maximum number of samples per group (default = 70).')
+
+    # training & testing
+    parser.add_argument('-e', '--max_epochs', type=int, default=10, help='maximum number of epochs to train (default = 10)')
+    parser.add_argument('--bs_mini', type=int, default=32, help='number of regression datasets per training minibatch (default = 32)')
+    parser.add_argument('--lr', type=float, default=1e-3, help='optimizer learning rate (default = 1e-3)')
+    parser.add_argument('--test_interval', type=int, default=5, help='sample posterior every #n epochs (default = 5)')
+    # parser.add_argument('--patience', type=int, default=20, help='early stopping criterium (default = 20)')
+
+    return parser.parse_args()
+
+# -----------------------------------------------------------------------------
