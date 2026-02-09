6c3920e47699949065745ba05b2a04885fa9d7e4
adkipnis <adkipnis@mailbox.org>
2026-02-09 20:54:07 +0100

    sample moments

--- metabeta/evaluation/moments.py
+++ metabeta/evaluation/moments.py
@@ -12,3 +12,32 @@ def numFixed(proposed: Proposed) -> int:
     d = D - q - 1
     return int(d)
 
+def sampleMean(proposed: Proposed) -> dict[str, torch.Tensor]:
+    d = numFixed(proposed)
+    global_mean = proposed['global']['samples'].mean(-2)
+    ffx_mean = global_mean[..., :d]
+    sigma_rfx_mean = global_mean[..., d:-1]
+    sigma_eps_mean = global_mean[..., -1]
+    rfx_mean = proposed['local']['samples'].mean(-2)
+    return {
+        'ffx': ffx_mean,
+        'sigma_rfx': sigma_rfx_mean,
+        'sigma_eps': sigma_eps_mean,
+        'rfx': rfx_mean,
+    }
+
+# def sampleStd(proposed: Proposed) -> dict[str, torch.Tensor]:
+#     d = numFixed(proposed)
+#     global_std = proposed['global']['samples'].std(-2)
+#     ffx_std = global_std[..., :d]
+#     sigma_rfx_std = global_std[..., d:-1]
+#     sigma_eps_std = global_std[..., -1]
+#     rfx_std = proposed['local']['samples'].std(-2)
+#     return {
+#         'ffx': ffx_std,
+#         'sigma_rfx': sigma_rfx_std,
+#         'sigma_eps': sigma_eps_std,
+#         'rfx': rfx_std,
+#     }
+
+def getMasks(data: dict[str, torch.Tensor]) -> dict[str, torch.Tensor | None]:
