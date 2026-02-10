834d873437f7eb551ab51eaee507f84b5fd7bf22
adkipnis <adkipnis@mailbox.org>
2026-02-10 20:51:29 +0100

    use torch.quantile instead of manual sorting and index calculation

--- metabeta/evaluation/intervals.py
+++ metabeta/evaluation/intervals.py
@@ -2,40 +2,26 @@ import torch
 from metabeta.utils.evaluation import Proposed, numFixed, getMasks
 
 
-def sortProposed(proposed: Proposed) -> Proposed:
-    for source in ('global', 'local'):
-        samples = proposed[source]['samples']
-        log_prob = proposed[source]['log_prob']
-        sorted_samples, idx = samples.sort(dim=-2, descending=False)
-        sorted_log_prob = torch.gather(log_prob, -1, idx[..., 0])
-        proposed[source]['samples'] = sorted_samples
-        proposed[source]['log_prob'] = sorted_log_prob
-        # TODO: weights
-    return proposed
-
-
 def getQuantiles(
     roots: tuple[float, float],
-    sorted_samples: torch.Tensor,
+    samples: torch.Tensor,
     weights: torch.Tensor | None = None,
 ) -> torch.Tensor:
     assert len(roots) == 2
     assert 0.0 <= roots[0] < roots[1] <= 1.0
-    s = sorted_samples.shape[-2]
-    roots_ = torch.tensor(roots)
+    roots_ = torch.tensor(roots, device=samples.device)
     if weights is None:
-        root_idx = (roots_ * s).round().int().clamp_max(s - 1)
-        quantiles = sorted_samples[..., root_idx, :]
+        quantiles = torch.quantile(samples, roots_, dim=-2)
+        quantiles = quantiles.movedim(0, -2)
     else:
         raise NotImplementedError()
-    return quantiles
+    return quantiles # (b, ..., 2, d)
 
 
 def sampleCredibleInterval(
     proposed: Proposed,
     alpha: float,
 ) -> dict[str, torch.Tensor]:
-    proposed = sortProposed(proposed)
     d = numFixed(proposed)
     out = {}
     roots = (alpha / 2, 1 - alpha / 2)
