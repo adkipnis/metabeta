40a8014ea78069395f5356791896d09e91466feb
adkipnis <adkipnis@mailbox.org>
2026-02-10 18:19:16 +0100

    function for posterior predictive distribution

--- metabeta/evaluation/predictive.py
+++ metabeta/evaluation/predictive.py
@@ -2,3 +2,28 @@ import torch
 from torch import distributions as D
 from metabeta.utils.evaluation import Proposed, numFixed
 
+def samplePosteriorPredictive(
+    proposed: Proposed,
+    data: dict[str, torch.Tensor],
+) -> D.Normal:
+    ''' get p(y|X, theta) '''
+    d = numFixed(proposed)
+
+    # parameters
+    ffx = proposed['global']['samples'][..., :d] # (b, s, d)
+    sigma_eps = proposed['global']['samples'][..., -1] # (b, s)
+    rfx = proposed['local']['samples'] # (b, m, s, q)
+
+    # observations
+    X = data['X'] # (b, m, n, d)
+    Z = data['Z'] # (b, m, n, q)
+
+    # posterior params
+    mu_g = torch.einsum('bmnd,bsd->bmns', X, ffx)
+    mu_l = torch.einsum('bmnq,bmsq->bmns', Z, rfx)
+    loc = mu_g + mu_l
+    scale = sigma_eps.unsqueeze(1).unsqueeze(1) + 1e-12
+    return D.Normal(loc=loc, scale=scale)
+
+
+def posteriorPredictiveNLL(
