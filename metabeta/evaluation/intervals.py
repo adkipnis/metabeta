0dd6443bb2d5a0e08e9a21f9be43ed715c1e9799
adkipnis <adkipnis@mailbox.org>
2026-02-10 17:42:42 +0100

    cosmetics

--- metabeta/evaluation/intervals.py
+++ metabeta/evaluation/intervals.py
@@ -24,7 +24,7 @@ def getQuantiles(
     s = sorted_samples.shape[-2]
     roots_ = torch.tensor(roots)
     if weights is None:
-        root_idx = (roots_ * s).round().int().clamp_max(s-1)
+        root_idx = (roots_ * s).round().int().clamp_max(s - 1)
         quantiles = sorted_samples[..., root_idx, :]
     else:
         raise NotImplementedError()
@@ -86,29 +86,26 @@ def sampleCoverageError(
             gt = gt.unsqueeze(-1)
             ci = ci.unsqueeze(-1)
         out[key] = coverage(ci, gt, mask) - nominal
-        if key == 'rfx': # average over groups
+        if key == 'rfx':   # average over groups
             out[key] = out[key].mean(0)
-        out[key] = out[key].mean(0).item() # TODO: incorporate mask?
+        out[key] = out[key].mean(0).item()   # TODO: incorporate mask?
     return out
 
 
 def expectedCoverageError(
     proposed: Proposed,
     data: dict[str, torch.Tensor],
-    alphas: list[float] = [0.02, 0.05, 0.10, 0.20, .32, 0.50],
+    alphas: list[float] = [0.02, 0.05, 0.10, 0.20, 0.32, 0.50],
 ) -> dict[str, float]:
     # get coverage error for each alpha level
     ce_dict = {}
     for alpha in alphas:
         ci = sampleCredibleInterval(proposed, alpha=alpha)
-        ce_dict[alpha] = sampleCoverageError(ci, data, nominal=1-alpha)
+        ce_dict[alpha] = sampleCoverageError(ci, data, nominal=(1 - alpha))
 
     # average over alphas
     keys = next(iter(ce_dict.values())).keys()
-    out = {
-        k: sum(d[k] for d in ce_dict.values()) / len(ce_dict)
-        for k in keys
-    }
+    out = {k: sum(d[k] for d in ce_dict.values()) / len(ce_dict) for k in keys}
     return out
 
 
