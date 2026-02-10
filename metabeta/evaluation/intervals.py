90afda8a473d8a647f0c54cd4ea394ad4438ed3f
adkipnis <adkipnis@mailbox.org>
2026-02-10 16:24:43 +0100

    sort proposed dict once before interval search

--- metabeta/evaluation/intervals.py
+++ metabeta/evaluation/intervals.py
@@ -2,3 +2,15 @@ import torch
 from metabeta.utils.evaluation import Proposed, numFixed, getMasks
 
 
+def sortProposed(proposed: Proposed) -> Proposed:
+    for source in ('global', 'local'):
+        samples = proposed[source]['samples']
+        log_prob = proposed[source]['log_prob']
+        sorted_samples, idx = samples.sort(dim=-2, descending=False)
+        sorted_log_prob = torch.gather(log_prob, -1, idx[..., 0])
+        proposed[source]['samples'] = sorted_samples
+        proposed[source]['log_prob'] = sorted_log_prob
+        # TODO: weights
+    return proposed
+
+
