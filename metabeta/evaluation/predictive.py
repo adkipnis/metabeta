8bf875cc7ac361e4291ce02192eecb5e76bf4ad1
adkipnis <adkipnis@mailbox.org>
2026-02-14 16:34:50 +0100

    blue format

--- metabeta/evaluation/predictive.py
+++ metabeta/evaluation/predictive.py
@@ -2,19 +2,20 @@ import torch
 from torch import distributions as D
 from metabeta.utils.evaluation import Proposal
 
+
 def getPosteriorPredictive(
     proposal: Proposal,
     data: dict[str, torch.Tensor],
 ) -> D.Normal:
-    ''' get p(y|X, theta) '''
+    """get p(y|X, theta)"""
     # parameters
-    ffx = proposal.ffx # (b, s, d)
-    sigma_eps = proposal.sigma_eps # (b, s)
-    rfx = proposal.rfx # (b, m, s, q)
+    ffx = proposal.ffx   # (b, s, d)
+    sigma_eps = proposal.sigma_eps   # (b, s)
+    rfx = proposal.rfx   # (b, m, s, q)
 
     # observations
-    X = data['X'] # (b, m, n, d)
-    Z = data['Z'] # (b, m, n, q)
+    X = data['X']   # (b, m, n, d)
+    Z = data['Z']   # (b, m, n, q)
 
     # posterior params
     mu_g = torch.einsum('bmnd,bsd->bmns', X, ffx)
@@ -29,11 +30,11 @@ def posteriorPredictiveNLL(
     data: dict[str, torch.Tensor],
 ) -> torch.Tensor:
     mask = data['mask_n'].unsqueeze(-1)
-    y = data['y'] # (b, m, n)
+    y = data['y']   # (b, m, n)
     nll = -pp.log_prob(y.unsqueeze(-1))
     nll = nll * mask
-    nll = nll.sum(dim=(1,2)) / mask.sum(dim=(1,2))
-    return nll # (b, s)
+    nll = nll.sum(dim=(1, 2)) / mask.sum(dim=(1, 2))
+    return nll   # (b, s)
 
 
 def posteriorPredictiveSample(
@@ -44,4 +45,3 @@ def posteriorPredictiveSample(
     y_rep = pp.sample((1,)).squeeze(0)
     y_rep = y_rep * mask
     return y_rep
-
