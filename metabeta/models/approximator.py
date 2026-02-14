aad153895e294976bc214581dfe8fddfe30de151
adkipnis <adkipnis@mailbox.org>
2026-02-14 16:40:58 +0100

    blue format

--- metabeta/models/approximator.py
+++ metabeta/models/approximator.py
@@ -34,7 +34,7 @@ class Approximator(nn.Module):
         else:
             raise NotImplementedError('unknown summarizer type')
         d_input_l = 1 + (d_ffx - 1) + (d_rfx - 1)
-        d_input_g = s_cfg.d_output + 1 # n_obs per group
+        d_input_g = s_cfg.d_output + 1   # n_obs per group
         self.summarizer_l = Summarizer(d_input=d_input_l, **s_cfg.to_dict())
         self.summarizer_g = Summarizer(d_input=d_input_g, **s_cfg.to_dict())
 
@@ -44,13 +44,13 @@ class Approximator(nn.Module):
             Posterior = CouplingFlow
         else:
             raise NotImplementedError('unknown posterior type')
-        d_var = 1 + d_rfx # number of variance params
-        d_prior = 2 * d_ffx + d_var # number of prior params
-        d_context_g = s_cfg.d_output + d_prior + 2 # global summary, prior, n_groups, n_total
-        d_context_l = d_ffx + d_var + d_input_g # global params, local summaries
-        self.posterior_g = Posterior(d_ffx+d_var, d_context_g, **p_cfg.to_dict())
+        d_var = 1 + d_rfx   # number of variance params
+        d_prior = 2 * d_ffx + d_var   # number of prior params
+        d_context_g = s_cfg.d_output + d_prior + 2   # global summary, prior, n_groups, n_total
+        d_context_l = d_ffx + d_var + d_input_g   # global params, local summaries
+        self.posterior_g = Posterior(d_ffx + d_var, d_context_g, **p_cfg.to_dict())
         self.posterior_l = Posterior(max(d_rfx, 2), d_context_l, **p_cfg.to_dict())
- 
+
     def compile(self) -> None:
         self.summarizer_l = torch.compile(self.summarizer_l)
         self.summarizer_g = torch.compile(self.summarizer_g)
@@ -64,14 +64,14 @@ class Approximator(nn.Module):
     @property
     def dtype(self):
         return next(self.parameters()).dtype
- 
+
     @property
     def n_params(self) -> int:
         param_counts = (p.numel() for p in self.parameters() if p.requires_grad)
         return sum(param_counts)
 
     def _inputs(self, data: dict[str, torch.Tensor]) -> torch.Tensor:
-        ''' get summarizer inputs '''
+        """get summarizer inputs"""
         d, q = self.d_ffx, self.d_rfx
         y = data['y'].unsqueeze(-1)
         X = data['X'][..., 1:d]
@@ -81,11 +81,11 @@ class Approximator(nn.Module):
     def _targets(
             self, data: dict[str, torch.Tensor], local: bool = False,
     ) -> torch.Tensor:
-        ''' get posterior targets '''
+        """get posterior targets"""
         if local:
             targets = data['rfx']
-            if self.d_rfx == 1: # handle 1D local params for flow
-                targets = F.pad(targets, (0,1))
+            if self.d_rfx == 1:   # handle 1D local params for flow
+                targets = F.pad(targets, (0, 1))
             return targets
         targets = [data['ffx'], data['sigma_rfx'], data['sigma_eps'].unsqueeze(-1)]
         return torch.cat(targets, dim=-1)
@@ -93,11 +93,11 @@ class Approximator(nn.Module):
     def _masks(
             self, data: dict[str, torch.Tensor], local: bool = False,
     ) -> torch.Tensor:
-        ''' get masks for the posterior targets '''
+        """get masks for the posterior targets"""
         if local:
             mask_q = data['mask_q']
-            if mask_q.shape[-1] == 1: # handle 1D local params for flow
-                mask_q = F.pad(mask_q, (0,1))
+            if mask_q.shape[-1] == 1:   # handle 1D local params for flow
+                mask_q = F.pad(mask_q, (0, 1))
             return data['mask_m'].unsqueeze(-1) & mask_q.unsqueeze(-2)
         masks = [
             data['mask_d'],
@@ -111,9 +111,9 @@ class Approximator(nn.Module):
         summary: torch.Tensor,
         data: dict[str, torch.Tensor],
         local: bool = False,
-        numerator: int = 10, # for normalizing counts
+        numerator: int = 10,  # for normalizing counts
     ) -> torch.Tensor:
-        ''' append summary with selected metadata '''
+        """append summary with selected metadata"""
         out = [summary]
         if local:
             n_obs = data['ns'].unsqueeze(-1).sqrt() / numerator
@@ -136,18 +136,18 @@ class Approximator(nn.Module):
     ) -> torch.Tensor:
         b, m, _ = local_summary.shape
         ndim = global_params.dim()
-        if ndim == 3: # samples
+        if ndim == 3:   # samples
             s = global_params.shape[-2]
             global_params = global_params.unsqueeze(1).expand(b, m, s, -1)
             local_summary = local_summary.unsqueeze(2).expand(b, m, s, -1)
-        elif ndim == 2: # ground truth
+        elif ndim == 2:   # ground truth
             global_params = global_params.unsqueeze(1).expand(b, m, -1)
         else:
             raise ValueError(f'global_params has {ndim} dims, expected 2 or 3')
         return torch.cat([local_summary, global_params], dim=-1)
 
     def _preprocess(self, targets: torch.Tensor, local: bool = False) -> torch.Tensor:
-        ''' constrain target parameters '''
+        """constrain target parameters"""
         if local:
             return targets
         targets = targets.clone()
@@ -160,7 +160,7 @@ class Approximator(nn.Module):
         return targets
 
     def _postprocess(self, proposed: dict[str, dict[str, torch.Tensor]]):
-        ''' reverse _preprocess for samples '''
+        """reverse _preprocess for samples"""
         d = self.d_ffx
         q = self.d_rfx
 
@@ -175,8 +175,7 @@ class Approximator(nn.Module):
             samples = proposed['global']['samples'].clone()
             sigmas = samples[..., d:]
             # Jacobian for sigma dims: d softplus / dx = sigmoid(x)
-            log_det = torch.where(
-                sigmas != 0, F.logsigmoid(sigmas), torch.zeros_like(sigmas))
+            log_det = torch.where(sigmas != 0, F.logsigmoid(sigmas), torch.zeros_like(sigmas))
             proposed['global']['log_prob'] -= log_det.sum(dim=-1)
             samples[..., d:] = maskedSoftplus(sigmas)
             proposed['global']['samples'] = samples
@@ -193,7 +192,7 @@ class Approximator(nn.Module):
         return summary_g, summary_l
 
     def forward(self, data: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
-        ''' training method: learn conditional forward pass '''
+        """training method: learn conditional forward pass"""
         loss = {}
 
         # summaries
@@ -203,15 +202,16 @@ class Approximator(nn.Module):
         targets_g = self._targets(data, local=False)
         mask_g = self._masks(data, local=False)
         targets_g = self._preprocess(targets_g, local=False)
-        loss['global'] = self.posterior_g.loss(    # type: ignore
-            targets_g, context=summary_g, mask=mask_g)
+        loss['global'] = self.posterior_g.loss(  # type: ignore
+            targets_g, context=summary_g, mask=mask_g
+        )
 
         # local posterior
         targets_l = self._targets(data, local=True)
         targets_l = self._preprocess(targets_l, local=True)
         mask_l = self._masks(data, local=True)
         context_l = self._localContext(summary_l, targets_g)
-        loss['local'] = self.posterior_l.loss(    # type: ignore
+        loss['local'] = self.posterior_l.loss(  # type: ignore
             targets_l, context=context_l, mask=mask_l
         )
 
@@ -223,15 +223,16 @@ class Approximator(nn.Module):
     def estimate(
             self, data: dict[str, torch.Tensor], n_samples: int = 100,
     ) -> Proposal:
-        ''' inference method: sample and apply conditional backward pass '''
+        """inference method: sample and apply conditional backward pass"""
         assert n_samples > 0, 'n_samples must be positive'
         proposed = {}
         summary_g, summary_l = self._summarize(data)
 
         # global posterior
         mask_g = self._masks(data, local=False)
-        samples_g, log_prob_g = self.posterior_g.sample(    # type: ignore
-            n_samples, context=summary_g, mask=mask_g)
+        samples_g, log_prob_g = self.posterior_g.sample(  # type: ignore
+            n_samples, context=summary_g, mask=mask_g
+        )
         proposed['global'] = {'samples': samples_g, 'log_prob': log_prob_g}
 
         # local posterior
@@ -239,8 +240,9 @@ class Approximator(nn.Module):
         b, m, q = mask_l.shape
         mask_l = mask_l.unsqueeze(-2).expand(b, m, n_samples, q)
         context_l = self._localContext(summary_l, samples_g)
-        samples_l, log_prob_l = self.posterior_l.sample(    # type: ignore
-            1, context=context_l, mask=mask_l)
+        samples_l, log_prob_l = self.posterior_l.sample(  # type: ignore
+            1, context=context_l, mask=mask_l
+        )
         samples_l, log_prob_l = samples_l.squeeze(-2), log_prob_l.squeeze(-1)
         proposed['local'] = {'samples': samples_l, 'log_prob': log_prob_l}
 
@@ -248,11 +250,13 @@ class Approximator(nn.Module):
         proposal = self._postprocess(proposed)
         return proposal
 
+
 # =============================================================================
 if __name__ == '__main__':
     from pathlib import Path
     from metabeta.utils.dataloader import Dataloader
     from metabeta.utils.config import dataFromYaml, modelFromYaml
+
     torch.manual_seed(0)
 
     # load toy data
@@ -264,7 +268,7 @@ if __name__ == '__main__':
 
     # init toy model
     model_cfg_path = Path('..', 'models', 'configs', 'toy.yaml')
-    model_cfg = modelFromYaml(model_cfg_path, dl.dataset.d, dl.dataset.q) # type: ignore
+    model_cfg = modelFromYaml(model_cfg_path, dl.dataset.d, dl.dataset.q)   # type: ignore
     model = Approximator(model_cfg)
     # model.compile()
 
