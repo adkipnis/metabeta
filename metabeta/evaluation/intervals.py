f3d4cea2c816ee3b29f3e2138881c89dfabaa1f8
adkipnis <adkipnis@mailbox.org>
2026-02-20 22:31:35 +0100

    spread ALPHAS more evenly

--- metabeta/evaluation/intervals.py
+++ metabeta/evaluation/intervals.py
@@ -2,6 +2,8 @@ import torch
 from metabeta.utils.evaluation import Proposal, getMasks, weightedQuantile
 
 EPS = 1e-6
+ALPHAS = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+
 
 def getQuantiles(
     roots: tuple[float, float],
