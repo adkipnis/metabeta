59ec4599de959da76c9ea7670bef3ef1de7f2da9
adkipnis <adkipnis@mailbox.org>
2026-02-23 21:11:05 +0100

    simultaneous bands based on draws form U

--- metabeta/plot/sbc.py
+++ metabeta/plot/sbc.py
@@ -46,6 +46,37 @@ def pointwiseBands(
     return z, lower, upper
 
 
+def simultaneousBands(
+    n_eff: int,  # effective number of posteriors
+    alpha: float = 0.05,  # alpha error for bounds
+    diff: bool = False,  # use ECDF delta
+    max_n: int = 1000,  # upper bound for n_eff
+    n_sim: int = 2000,  # number of draws from the uniform
+    eps: float = 1e-5,
+) -> tuple[np.ndarray, ...]:
+    """Simultanous ECDF bands as proposed by Sailynoja et al. (2022)"""
+    K = min(n_eff, max_n)
+    z = np.linspace(0.0 + eps, 1.0 - eps, K)
+    u = np.random.uniform(size=(n_sim, n_eff))
+    gammas = _minimalCoverageProbs(z, u)
+    gamma = np.percentile(gammas, 100.0 * alpha)
+    lower = binom(n_eff, z).ppf(gamma / 2.0) / n_eff
+    upper = binom(n_eff, z).ppf(1.0 - gamma / 2.0) / n_eff
+    if diff:
+        lower -= z
+        upper -= z
+    return z, lower, upper
+
+
+def _minimalCoverageProbs(z: np.ndarray, u: np.ndarray) -> np.ndarray:
+    """gamma per for n_sim simulations"""
+    n_eff = u.shape[1]
+    # ECDF values of each simulated sample at each z
+    F_m = np.sum(z[:, None] >= u[:, None, :], axis=-1) / n_eff
+    bin1 = binom(n_eff, z).cdf(n_eff * F_m)
+    bin2 = binom(n_eff, z).cdf(n_eff * F_m - 1)
+    gamma = 2 * np.min(np.min(np.stack([bin1, 1 - bin2], axis=-1), axis=-1), axis=-1)
+    return gamma
 
 
 def _plotSbcEcdf(
