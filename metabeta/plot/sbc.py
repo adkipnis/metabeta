f9bc8b9a8a85e895a4b8fc3c13b265071c4e980f
adkipnis <adkipnis@mailbox.org>
2026-02-23 17:50:45 +0100

    main plotter

--- metabeta/plot/sbc.py
+++ metabeta/plot/sbc.py
@@ -46,3 +46,42 @@ def theoreticalLimits(
     return p, lower, upper
 
 
+def _plotSbcEcdf(
+    ax: Axes,
+    ranks: torch.Tensor,
+    names: list[str],
+    mask: torch.Tensor | None,
+    diff: bool = False,
+    upper: bool = True,
+    lower: bool = True,
+) -> None:
+    assert len(names) == ranks.shape[-1], 'shape mismatch'
+    for i, name in enumerate(names):
+        if mask is not None:
+            mask_i = mask[..., i]
+            x = ranks[mask_i, i].sort()[0].numpy()
+        else:
+            x = ranks.view(-1, ranks.shape[-1])[:, i].sort()[0].numpy()
+        x = np.pad(x, (1, 1), constant_values=(0, 1))
+        y = np.linspace(0, 1, num=x.shape[-1])
+        if diff:
+            y = y - x
+        ax.plot(x, y, label=name, lw=3)
+
+    ax.set_axisbelow(True)
+    ax.grid(True)
+    ax.set_xlim(-0.02, 1.02)
+    ax.tick_params(axis='both', labelsize=18)
+    prefix = r'$\Delta$ ' if diff else ''
+    ax.set_ylabel(f'{prefix}ECDF', fontsize=26, labelpad=10)
+    if upper:
+        ax.set_title('SBC', fontsize=28, pad=15)
+        ax.legend(fontsize=18, markerscale=2.5, loc='upper left')
+    if lower:
+        ax.set_xlabel('fractional rank', fontsize=26, labelpad=10)
+    else:
+        ax.set_xlabel('')
+        ax.tick_params(axis='x', labelcolor='w', size=1)
+
+
+def plotSBC(
