8b8f16507a702cfb98d0a783b8e6ca5bc4b870ba
adkipnis <adkipnis@mailbox.org>
2026-02-23 16:06:21 +0100

    separate dir for plotting

---
+++ metabeta/plot/__init__.py
@@ -0,0 +1,11 @@
+from .correlation import plotCorrelation
+from .coverage import plotCoverage
+from .dataset import plotDataset
+from .recovery import plotRecovery
+
+__all__ = [
+    'plotCorrelation',
+    'plotCoverage',
+    'plotDataset',
+    'plotRecovery',
+]
