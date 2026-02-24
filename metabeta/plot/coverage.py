950a8ad06dc493cdac01596d607cf2fd8fb66933
adkipnis <adkipnis@mailbox.org>
2026-02-24 14:59:31 +0100

    nicefy

--- metabeta/plot/coverage.py
+++ metabeta/plot/coverage.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 
 from metabeta.utils.evaluation import EvaluationSummary, getNames, Proposal, dictMean
-from metabeta.utils.plot import PALETTE, savePlot
+from metabeta.utils.plot import PALETTE, savePlot, niceify
 
 
 def _plotCoverage(
@@ -26,39 +26,25 @@ def _plotCoverage(
         coverage_i = 100.0 * values
         ax.plot(nominal, coverage_i, label=names[i], color=PALETTE[i], alpha=0.8, lw=3)
 
-    # stats info
-    txt = [f'{k} = {v:.2f}%' for k, v in stats.items()]
-    ax.text(
-        0.70,
-        0.05,
-        '\n'.join(txt),
-        transform=ax.transAxes,
-        ha='center',
-        va='bottom',
-        fontsize=20,
-        bbox=dict(
-            boxstyle='round',
-            facecolor=(1, 1, 1, 0.7),
-            edgecolor=(0, 0, 0, 0.15),
-        ),
-    )
-
     # final touches
     limits = (min(nominal), max(nominal))
     ax.plot(limits, limits, '--', lw=2, zorder=1, color='grey')
     ax.grid(True)
     ax.set_xticks(nominal)
     ax.set_yticks(nominal)
-    ax.tick_params(axis='both', labelsize=18)
-    ax.set_ylabel('Observed', fontsize=26, labelpad=10)
-    if upper:
-        ax.set_title('Coverage', fontsize=28, pad=15)
-        ax.legend(fontsize=18, loc='upper left')
-        if not lower:
-            ax.set_xlabel('')
-            ax.tick_params(axis='x', labelcolor='w', size=1)
-    if lower:
-        ax.set_xlabel('Nominal', fontsize=26, labelpad=10)
+
+    # niceify
+    info = {
+        'title': 'Credible Intervals',
+        'ylabel': 'Observed',
+        'xlabel': 'Nominal',
+        'show_title': upper,
+        'show_legend': upper,
+        'show_x': lower,
+        'stats': stats,
+        'stats_suffix': '%',
+        }
+    niceify(ax, info)
 
 
 def plotCoverage(
