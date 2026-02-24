cf36b61b1965b9f48dddc7278e951d5555c3558f
adkipnis <adkipnis@mailbox.org>
2026-02-24 15:20:00 +0100

    incorporate importance weights

--- metabeta/evaluation/sbc.py
+++ metabeta/evaluation/sbc.py
@@ -8,22 +8,30 @@ from metabeta.utils.evaluation import Proposal
 def fractionalRanks(
     samples: torch.Tensor,  # (b, ..., s, d)
     targets: torch.Tensor,  # (b, ..., d)
+    weights: torch.Tensor | None = None, # (b, s)
 ) -> torch.Tensor:
     sample_dim = -1 if targets.dim() == 1 else -2
     targets = targets.unsqueeze(sample_dim)
-    smaller = samples < targets
-    return smaller.float().mean(sample_dim)
+    smaller = (samples < targets).float()
+    if weights is None:
+        return smaller.mean(sample_dim)
+    if sample_dim == -2:
+        weights = weights.unsqueeze(-1)
+    if targets.dim() == 4:
+        weights = weights.unsqueeze(1)
+    return (smaller * weights).sum(sample_dim)
 
 
 def getFractionalRanks(
     proposal: Proposal, data: dict[str, torch.Tensor]
 ) -> dict[str, torch.Tensor]:
     param_names = ('ffx', 'sigma_rfx', 'sigma_eps', 'rfx')
+    weights = proposal.weights
     ranks = {}
     for param in param_names:
         samples = getattr(proposal, param)
         targets = data[param]
-        ranks[param] = fractionalRanks(samples, targets)
+        ranks[param] = fractionalRanks(samples, targets, weights)
     return ranks
 
 
