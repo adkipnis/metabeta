fd232a2c47246a5799eb22a4e8ad97cbacde7622
adkipnis <adkipnis@mailbox.org>
2026-03-04 15:48:42 +0100

    setup

--- metabeta/training/hyperoptimize.py
+++ metabeta/training/hyperoptimize.py
@@ -15,3 +15,19 @@ from metabeta.utils.logger import setupLogging
 logger = logging.getLogger('hyperoptimize.py')
 
 
+def setup() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+    parser.add_argument('--name', type=str, default='hyper', help='load configs/{name}.yaml')
+    parser.add_argument('--n_trials', type=int, default=3)
+    parser.add_argument('--sampler', type=str, default='tpe', choices=['tpe', 'random'])
+    parser.add_argument('--pruner', type=str, default='median', choices=['median', 'none'])
+
+    args = parser.parse_args()
+    path = Path(__file__).resolve().parent / 'configs' / f'{args.name}.yaml'
+    with open(path, 'r') as p:
+        cfg = yaml.safe_load(p)
+    cfg.update(vars(args))
+    return argparse.Namespace(**cfg)
+
+
+class HyperOptimizer:
