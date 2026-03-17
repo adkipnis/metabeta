8b56f6dafed0e0743d4045f6e9cb2d5e99172ff5
adkipnis <adkipnis@mailbox.org>
2026-03-17 11:40:38 +0100

    hashbang

--- scripts/simulation/generate.sh
+++ scripts/simulation/generate.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=gen-toy
+#SBATCH --output=logs/gen-toy/%j.out
+#SBATCH --error=logs/gen-toy/%j.err
+
 #SBATCH --partition cpu_p
 #SBATCH --qos cpu_normal
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --time=24:00:00
-#SBATCH --job-name=gen-toy
-#SBATCH --output=logs/gen-toy/%j.out
-#SBATCH --error=logs/gen-toy/%j.err
 
 source $HOME/.bashrc
 cd $HOME/metabeta/metabeta/simulation
