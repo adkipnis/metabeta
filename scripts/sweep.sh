fdca56018d763e8aae75d2f65511f3ee053eaa3d
adkipnis <adkipnis@mailbox.org>
2026-03-19 10:53:55 +0100

    third sweep

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=sweep-2
-#SBATCH --output=logs/sweep-2/%A_%a.out
-#SBATCH --error=logs/sweep-2/%A_%a.err
+#SBATCH --job-name=sweep-3
+#SBATCH --output=logs/sweep-3/%A_%a.out
+#SBATCH --error=logs/sweep-3/%A_%a.err
 #SBATCH --array=0-2
 
 #SBATCH --partition gpu_p
@@ -15,4 +15,4 @@
 source $HOME/.bashrc
 source $HOME/metabeta/.venv/bin/activate
 cd $HOME/metabeta/metabeta/training
-python train.py --name cluster --m_tag sweep_2_${SLURM_ARRAY_TASK_ID}
+python train.py --name cluster --m_tag sweep_3_${SLURM_ARRAY_TASK_ID}
