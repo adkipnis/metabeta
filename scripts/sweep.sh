6ced84fa45ee5aad41d83613c3e5ab33e0557fe1
adkipnis <adkipnis@mailbox.org>
2026-03-19 10:41:04 +0100

    second sweep

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=sweep-1
-#SBATCH --output=logs/sweep-1/%A_%a.out
-#SBATCH --error=logs/sweep-1/%A_%a.err
-#SBATCH --array=0-71
+#SBATCH --job-name=sweep-2
+#SBATCH --output=logs/sweep-2/%A_%a.out
+#SBATCH --error=logs/sweep-2/%A_%a.err
+#SBATCH --array=0-2
 
 #SBATCH --partition gpu_p
 #SBATCH --qos gpu_normal
@@ -15,4 +15,4 @@
 source $HOME/.bashrc
 source $HOME/metabeta/.venv/bin/activate
 cd $HOME/metabeta/metabeta/training
-python train.py --name cluster --m_tag sweep_1_${SLURM_ARRAY_TASK_ID}
+python train.py --name cluster --m_tag sweep_2_${SLURM_ARRAY_TASK_ID}
