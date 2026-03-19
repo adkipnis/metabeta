2942aff553f4e4cba020f41908d0adc90f4aa2e3
adkipnis <adkipnis@mailbox.org>
2026-03-19 16:14:36 +0100

    update sweep

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=sweep-4
-#SBATCH --output=logs/sweep-4/%A_%a.out
-#SBATCH --error=logs/sweep-4/%A_%a.err
-#SBATCH --array=0-4
+#SBATCH --job-name=sweep-5
+#SBATCH --output=logs/sweep-5/%A_%a.out
+#SBATCH --error=logs/sweep-5/%A_%a.err
+#SBATCH --array=0-1
 
 #SBATCH --partition gpu_p
 #SBATCH --qos gpu_normal
-#SBATCH --nodes=3
+#SBATCH --nodes=2
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --time=24:00:00
@@ -15,4 +15,4 @@
 source $HOME/.bashrc
 source $HOME/metabeta/.venv/bin/activate
 cd $HOME/metabeta/metabeta/training
-python train.py --name cluster --m_tag sweep_4_${SLURM_ARRAY_TASK_ID}
+python train.py --name cluster_${SLURM_ARRAY_TASK_ID} --m_tag affine
