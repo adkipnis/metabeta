bbabe25a384dae07c0944c8cd0bbecdf51a78e1d
adkipnis <adkipnis@mailbox.org>
2026-03-24 18:20:29 +0100

    switch to raise exception

--- metabeta/utils/padding.py
+++ metabeta/utils/padding.py
@@ -12,7 +12,8 @@ def maxShapes(batch: list[dict[str, np.ndarray]]) -> dict[str, tuple[int, ...]]:
             if key not in out:
                 out[key] = shape
                 continue
-            assert len(shape) == len(out[key]), 'ndim mismatch'
+            if len(shape) != len(out[key]):
+                raise ValueError(f'ndim mismatch for key={key}')
             # Expand out[key] tuple elementwise to max dimension
             out[key] = tuple(max(old_dim, new_dim) for old_dim, new_dim in zip(out[key], shape))
     return out
