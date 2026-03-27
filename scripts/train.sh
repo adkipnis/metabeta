e3d7be5c8e2e01c7f36ca4ad8846912ebe221863
adkipnis <adkipnis@mailbox.org>
2026-03-27 15:56:24 +0100

    use wandb in training

--- scripts/train.sh
+++ scripts/train.sh
@@ -14,4 +14,4 @@
 source $HOME/.bashrc
 source $HOME/metabeta/.venv/bin/activate
 cd $HOME/metabeta/metabeta/training
-python train.py --name small-n -e 250
+python train.py --name small-n -e 250 --wandb
