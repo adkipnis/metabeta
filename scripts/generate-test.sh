3c07083527c59828de60b71b7d563f806b0d865e
adkipnis <adkipnis@mailbox.org>
2026-04-01 12:23:14 +0200

    move comments up

--- scripts/generate-test.sh
+++ scripts/generate-test.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        --d_tag) D_TAG="$2"; shift 2 ;;
-        *) echo "Unknown argument: $1"; exit 1 ;;
-    esac
-done
-[[ -z "$D_TAG" ]] && { echo "Usage: $0 --d_tag <tag>"; exit 1; }
-
 #SBATCH --job-name=generate-test
 #SBATCH --output=logs/generate-test/generate_%j.out
 #SBATCH --error=logs/generate-test/generate_%j.err
@@ -18,6 +10,15 @@ done
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --time=24:00:00
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --d_tag) D_TAG="$2"; shift 2 ;;
+        *) echo "Unknown argument: $1"; exit 1 ;;
+    esac
+done
+[[ -z "$D_TAG" ]] && { echo "Usage: $0 --d_tag <tag>"; exit 1; }
+
 
 source $HOME/.bashrc
 source $HOME/metabeta/.venv/bin/activate
