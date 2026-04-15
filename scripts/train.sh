62133a9b144efd169b03847c753d3f4764e5467c
adkipnis <adkipnis@mailbox.org>
2026-04-15 14:36:05 +0200

    update flags

--- scripts/train.sh
+++ scripts/train.sh
@@ -17,15 +17,33 @@ EPOCHS=1000
 
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --d_tag) D_TAG="$2"; shift 2 ;;
+        --tag) TAG="$2"; shift 2 ;;
+        --valid_ds_type) VALID_DS_TYPE="$2"; shift 2 ;;
         --epochs) EPOCHS="$2"; shift 2 ;;
         --seed) SEED="$2"; shift 2 ;;
         *) echo "Unknown argument: $1"; exit 1 ;;
     esac
 done
-[[ -z "$D_TAG" ]] && { echo "Usage: $0 --d_tag <tag> [--epochs N] [--seed N]"; exit 1; }
+[[ -z "$TAG" ]] && { echo "Usage: $0 --tag <size-family-ds_type> [--valid_ds_type <type>] [--epochs N] [--seed N]"; exit 1; }
+
+IFS='-' read -r SIZE FAM_NAME DS_TYPE <<< "$TAG"
+case $FAM_NAME in
+    n) FAMILY=0 ;;
+    b) FAMILY=1 ;;
+    p) FAMILY=2 ;;
+    *) echo "Unknown family letter: $FAM_NAME (use n, b, or p)"; exit 1 ;;
+esac
 
 source $HOME/.bashrc
 source $HOME/metabeta/.venv/bin/activate
 cd $HOME/metabeta/metabeta/training
-python train.py --name "${D_TAG}" -e ${EPOCHS} ${SEED:+--seed ${SEED}} --wandb --device cuda --no-plot
+python train.py \
+    --size "${SIZE}" \
+    --family ${FAMILY} \
+    --ds_type "${DS_TYPE}" \
+    ${VALID_DS_TYPE:+--valid_ds_type ${VALID_DS_TYPE}} \
+    -e ${EPOCHS} \
+    ${SEED:+--seed ${SEED}} \
+    --wandb \
+    --device cuda \
+    --no-plot
