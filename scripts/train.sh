5a0c71151e836205c600d1252578bdae74fe8428
adkipnis <adkipnis@mailbox.org>
2026-04-26 14:03:32 +0200

    reduce n gpus

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@
 
 #SBATCH --partition gpu_p
 #SBATCH --qos gpu_normal
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --constraint=[a100_20gb|a100_80gb|a100_40gb|h100_80gb]
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
