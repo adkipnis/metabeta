4e0e2c6eab8288d811334ad73b92cd72bbc937d5
adkipnis <adkipnis@mailbox.org>
2026-05-21 11:42:44 +0200

    update import

--- metabeta/simulation/check.py
+++ metabeta/simulation/check.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 from tqdm import tqdm
 
-from metabeta.simulation.fit import Fitter
+from metabeta.simulation.pymc import Fitter
 from metabeta.utils.names import datasetFilename
 
 # fmt: off
