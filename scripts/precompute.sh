acc21eb15ae1682041b54d296954734e2ed42c26
adkipnis <adkipnis@mailbox.org>
2026-07-26 22:24:23 +0200

    more modular script

--- scripts/precompute.sh
+++ scripts/precompute.sh
@@ -17,18 +17,22 @@
 CHUNK_SIZE=1
 START_EPOCH=1
 OVERWRITE=""
+EVAL_DS_TYPE="sampled"
+TRAIN_DS_TYPE="mixed"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         --family)    FAM_NAME="$2";    shift 2 ;;
         --size)      SIZE="$2";        shift 2 ;;
+        --ds_type)   EVAL_DS_TYPE="$2"; shift 2 ;;
+        --train_ds_type) TRAIN_DS_TYPE="$2"; shift 2 ;;
         --start)     START_EPOCH="$2"; shift 2 ;;
         --overwrite) OVERWRITE="--overwrite"; shift ;;
         *) echo "Unknown argument: $1"; exit 1 ;;
     esac
 done
 [[ -z "${FAM_NAME:-}" || -z "${SIZE:-}" ]] && {
-    echo "Usage: $0 --family <n|b|p> --size <size> [--start <epoch>] [--overwrite]"; exit 1
+    echo "Usage: $0 --family <n|b|p> --size <size> [--ds_type <sampled|real|...>] [--train_ds_type <mixed|...>] [--start <epoch>] [--overwrite]"; exit 1
 }
 
 case $FAM_NAME in
@@ -47,8 +51,8 @@ cd $HOME/metabeta/metabeta/analytical
 
 if [[ $SLURM_ARRAY_TASK_ID -lt $N_NON_TRAIN ]]; then
     PARTITION="${NON_TRAIN[$SLURM_ARRAY_TASK_ID]}"
-    python precompute.py --size "${SIZE}" --family ${FAMILY} --ds_type sampled --partition "${PARTITION}" ${OVERWRITE}
+    python precompute.py --size "${SIZE}" --family ${FAMILY} --ds_type "${EVAL_DS_TYPE}" --partition "${PARTITION}" ${OVERWRITE}
 else
     EPOCH=$(( START_EPOCH + (SLURM_ARRAY_TASK_ID - N_NON_TRAIN) * CHUNK_SIZE ))
-    python precompute.py --size "${SIZE}" --family ${FAMILY} --ds_type mixed --partition train --epoch ${EPOCH} ${OVERWRITE}
+    python precompute.py --size "${SIZE}" --family ${FAMILY} --ds_type "${TRAIN_DS_TYPE}" --partition train --epoch ${EPOCH} ${OVERWRITE}
 fi
